6944fea65d7077087050e3516d9803a85b2d79ac
apkretov <apkretov@hotmail.com>
2025-03-04 21:44:49 +0300

    Uploaded C#

---
+++ C#/Examples/Lambda expressions/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApplication1 {
+   class Program {
+      private delegate int TestDelegate(int a, int b);
+      private static int Add(int a, int b) { return a + b; }
+      private static int Subtract(int a, int b) { return a - b; }
+
+      static void Main(string[] args) {
+         TestDelegate ptrAdd = Add;
+         Console.WriteLine("{0}", ptrAdd(10, 20));
+         TestDelegate ptrSubstractExplicit = (int a, int b) => (a - b);
+         Console.WriteLine("{0}", ptrSubstractExplicit(50, 30));
+         TestDelegate ptrSubstractImplicit = (a, b) => (a - b);
+         Console.WriteLine("{0}", ptrSubstractImplicit(200, 50));
+      }
+   }
+}
---
+++ C#/Examples/namespace/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Test {
+   namespace Nested1 {
+      class Program {
+         static void Main(string[] args) { }
+      }
+    }
+   
+   namespace Nested2 {
+      class test {
+
+      }
+   }
+}
+
+namespace Test2.Dot1 {
+   class Test { }
+}
+
+namespace Test2.Dot2 {
+   class Test { }
+}
+
+namespace Test2.Dot2 {
+   class Test2 { }
+}
+
+class test {
+   Test.Nested2.test objTest;
+   Test2.Dot1.Test objTest2;
+   Test2.Dot2.Test objTest3;
+   Test2.Dot2.Test2 objTest4;
+}
