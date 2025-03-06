6eefdbfd24cf4214921036ec0b26d805adf8d1d2
apkretov <apkretov@hotmail.com>
2025-03-06 20:33:19 +0300

    08 Sharpen your pencil 1

---
+++ C++/Examples/Head First Design Patterns/01 Intro to Design Patterns/08 Sharpen your pencil 1.cpp
@@ -0,0 +1,37 @@
+#if 1
+
+#include <array>
+#include "../../stdafx.h"
+#include "04 Joe thinks about inheritance 2.h"
+using namespace std;
+
+struct FlyRocketPowered final : FlyBehavior {
+	void fly() const override { cout << "The duck flies rocket-powered.\n"; }
+};
+
+struct RocketDuck final : Duck {
+	RocketDuck() : Duck(make_unique<MuteQuack>(), make_unique<FlyRocketPowered>()) {}
+	void display() const override { cout << "The duck looks like a rocket duck.\n"; }
+};
+
+int main() {
+	print_file_line();
+
+	array<unique_ptr<Duck>, 5> ducks = {
+		make_unique<MallardDuck>(),
+		make_unique<RedheadDuck>(),
+		make_unique<RubberDuck>(),
+		make_unique<DecoyDuck>(),
+		make_unique<RocketDuck>()
+	};
+
+	for (const auto &duck : ducks) {
+		duck->quack();
+		duck->swim();
+		duck->display();
+		duck->fly();
+		cout << '\n';
+	}
+	return 0;
+}
+#endif 
