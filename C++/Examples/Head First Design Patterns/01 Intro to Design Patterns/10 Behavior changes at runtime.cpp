852987ad69a0e0aabcd022872d326d0cde4d35f3
apkretov <apkretov@hotmail.com>
2025-03-07 15:55:38 +0300

    10 Behavior changes at runtime.cpp

--- C++/Examples/Head First Design Patterns/01 Intro to Design Patterns/10 Behavior changes at runtime.cpp
+++ C++/Examples/Head First Design Patterns/01 Intro to Design Patterns/10 Behavior changes at runtime.cpp
@@ -1,25 +1,10 @@
-#if 0
+#if 1
 
 #include "../../stdafx.h"
 #include "04 Joe thinks about inheritance 2.h"
 using namespace std;
 
-/* Java
-public class Duck {
-	QuackBehavior quackBehavior;
-	public void performQuack() { quackBehavior.quack();	}
-}
-
-public class MallardDuck extends Duck {
-	public MallardDuck() {
-		quackBehavior = new Quack();
-		flyBehavior = new FlyWithWings();
-	}
-
-	public void display() {	System.out.println("I’m a real Mallard duck"); }
-}
-*/
-
+#pragma region MINE
 class Duck {
 	unique_ptr<QuackBehavior> quacking;
 	unique_ptr<FlyBehavior> flying;
@@ -30,21 +15,30 @@ public:
 	void swim() const { cout << "The duck swims.\n"; }
 	virtual void display() const = 0;
 	void performFly() const { flying->fly(); }
+	void setQuackBehavior(unique_ptr<QuackBehavior> quack_behavior) { quacking = std::move(quack_behavior); }
+	void setFlyBehavior(unique_ptr<FlyBehavior> fly_behavior) { flying = std::move(fly_behavior); }
 };
 
-struct MallardDuck final : Duck {
-	MallardDuck() : Duck(make_unique<Quack>(), make_unique<FlyWithWings>()) {}
-	void display() const override { cout << "I’m a real Mallard duck\n"; }
-};
+#include "04 Joe thinks about inheritance 4.h"
 
 int main() {
 	print_file_line();
 
+	auto enact_duck = [](const MallardDuck &duck) {
+		duck.performQuack();
+		duck.performFly();
+		duck.display();
+	};
+
 	MallardDuck duck;
-	duck.performQuack();
-	duck.performFly();
-	duck.display();
+	enact_duck(duck);
+
+	duck.setQuackBehavior(make_unique<Squeak>());
+	duck.setFlyBehavior(make_unique<FlyNoWay>());
+	cout << '\n';
+	enact_duck(duck);
 
 	return 0;
 }
+#pragma endregion MINE
 #endif //1
