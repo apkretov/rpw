73eba4e141b862687aaa393c35d7eaec53c591bb
apkretov <apkretov@hotmail.com>
2025-03-07 10:48:12 +0300

    08 Sharpen your pencil 2.cpp with HFDP's DuckCall

--- C++/Examples/Head First Design Patterns/01 Intro to Design Patterns/08 Sharpen your pencil 2.cpp
+++ C++/Examples/Head First Design Patterns/01 Intro to Design Patterns/08 Sharpen your pencil 2.cpp
@@ -1,10 +1,14 @@
 #if 1
 
+#undef MINE
+#define HFDP // Head First Design Patterns
+
 #include <array>
 #include "../../stdafx.h"
 #include "04 Joe thinks about inheritance 2.h"
 using namespace std;
 
+#ifdef MINE
 class DuckActor {
 	unique_ptr<QuackBehavior> quacking;
 public:
@@ -16,12 +20,12 @@ public:
 };
 
 struct MallardDuckActor final : DuckActor {
-	MallardDuckActor() : DuckActor(make_unique<Quack>()) {}
+	MallardDuckActor() : SoundDevice(make_unique<Quack>()) {}
 	void display() const override { cout << "The duck actor looks like a mallard.\n"; }
 };
 
 struct RubberDuckActor final : DuckActor {
-	RubberDuckActor() : DuckActor(make_unique<Squeak>()) {}
+	RubberDuckActor() : SoundDevice(make_unique<Squeak>()) {}
 	void display() const override { cout << "The duck actor looks like a rubber duck.\n"; }
 };
 
@@ -42,4 +46,28 @@ int main() {
 
 	return 0;
 }
+#endif //MINE
+
+#ifdef HFDP
+class SoundDevice {
+	unique_ptr<QuackBehavior> sounding;
+public:
+	SoundDevice(unique_ptr<QuackBehavior> quack_behavior) : sounding(std::move(quack_behavior)) {}
+	~SoundDevice() = default;
+	void sound() const { sounding->quack(); };
+};
+
+struct DuckCall final : SoundDevice {
+	DuckCall() : SoundDevice(make_unique<Quack>()) {}
+};
+
+int main() {
+	print_file_line();
+
+	DuckCall duck_call;
+	duck_call.sound();
+
+	return 0;
+}
+#endif //HFDP
 #endif 
