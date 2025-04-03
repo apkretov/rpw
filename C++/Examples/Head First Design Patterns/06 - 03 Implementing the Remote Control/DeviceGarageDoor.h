83e8f775b3459b60c491be64ea968f6d5af94875
apkretov <apkretov@hotmail.com>
2025-04-03 20:57:50 +0300

    Device names are used

--- C++/Examples/Head First Design Patterns/06 - 03 Implementing the Remote Control/DeviceGarageDoor.h
+++ C++/Examples/Head First Design Patterns/06 - 03 Implementing the Remote Control/DeviceGarageDoor.h
@@ -10,10 +10,10 @@ class GarageDoor {
 	string name_;
 public:
 	explicit GarageDoor(string_view name) : name_(name) {}
-	void up() { cout << "Garage Door is Open\n"; }
-	void down() { cout << "Garage Door is Closed\n"; }
-	void stop() { cout << "Garage Door is Stopped\n"; }
-	void lightOn() { cout << "Garage Light is On\n"; }
-	void lightOff() { cout << "Garage Light is Off\n"; }
+	void up() { cout << name_ << " Garage Door is Open\n"; }
+	void down() { cout << name_ << " Garage Door is Closed\n"; }
+	void stop() { cout << name_ << " Garage Door is Stopped\n"; }
+	void lightOn() { cout << name_ << " Garage Light is On\n"; }
+	void lightOff() { cout << name_ << " Garage Light is Off\n"; }
 };
 #pragma endregion //MINE
--- C++/Examples/Head First Design Patterns/06 - 03 Implementing the Remote Control/DeviceStereo.h
+++ C++/Examples/Head First Design Patterns/06 - 03 Implementing the Remote Control/DeviceStereo.h
@@ -8,11 +8,11 @@ class Stereo {
 	string name_;
 public:
 	explicit Stereo(string_view name) : name_(name) {}
-	void on() { cout << "Stereo is On\n"; }
-	void off() { cout << "Stereo is Off\n"; }
-	void setCD() { cout << "Stereo CD is Set\n"; }
-	void setDVD() { cout << "Stereo DVD is Set\n"; }
-	void setRadio() { cout << "Stereo Radio is Set\n"; }
-	void setVolume(size_t volume) { cout << "Stereo Volume is Set to " + volume + '\n'; }
+	void on() { cout << name_ << " Stereo is On\n"; }
+	void off() { cout << name_ << " Stereo is Off\n"; }
+	void setCD() { cout << name_ << " Stereo CD is Set\n"; }
+	void setDVD() { cout << name_ << " Stereo DVD is Set\n"; }
+	void setRadio() { cout << name_ << " Stereo Radio is Set\n"; }
+	void setVolume(size_t volume) { cout << name_ << " Stereo Volume is Set to " + volume + '\n'; }
 };
 #pragma endregion //MINE
