68e0e54303130a8d120e65f3e06943130b3b1896
apkretov <apkretov@hotmail.com>
2025-04-03 23:43:59 +0300

    CommandLightOff.h

--- C++/Examples/Head First Design Patterns/06 - 04 don’t forget undo/CommandLightOff.h
+++ C++/Examples/Head First Design Patterns/06 - 04 don’t forget undo/CommandLightOff.h
@@ -3,18 +3,20 @@
 #include "Command.h"
 #include "DeviceLight.h"
 
-#pragma region Implementing the Commands
+#pragma region 2 Let's start with the LightOnCommand
 /* Java
-public class LightOffCommand implements Command { 
+public class LightOffCommand implements Command {
 	Light light;
-	public LightOffCommand(Light light) { this.light = light; } 
-	public void execute() { light.off(); } // The LightOffCommand works exactly the same way as the LightOnCommand, except that we are binding the receiver to a different action : the off() method.
+	public LightOffCommand(Light light) { this.light = light; }
+	public void execute() { light.off(); }
+	public void undo() { light.on(); } // And here, undo() turns the light back on!
 }
 */
 class LightOffCommand final : public Command {
 	Light &light_;
 public:
-	explicit LightOffCommand(Light &light) : light_(light) {} 
-	void execute() override { light_.off(); } // The LightOffCommand works exactly the same way as the LightOnCommand, except that we are binding the receiver to a different action : the off() method.
+	explicit LightOffCommand(Light &light) : light_(light) {}
+	void execute() override { light_.off(); }
+	void undo() override { light_.on(); } // And here, undo() turns the light back on!
 };
-#pragma endregion //Implementing the Commands
+#pragma endregion //2 Let's start with the LightOnCommand
