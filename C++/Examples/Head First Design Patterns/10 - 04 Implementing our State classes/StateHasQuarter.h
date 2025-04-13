d156b6fe239028109f86f14970a2d1e2c0398bdd
apkretov <apkretov@hotmail.com>
2025-04-13 13:36:32 +0300

    Adding #pragme region's from the textbook

--- C++/Examples/Head First Design Patterns/10 - 04 Implementing our State classes/StateHasQuarter.h
+++ C++/Examples/Head First Design Patterns/10 - 04 Implementing our State classes/StateHasQuarter.h
@@ -9,29 +9,29 @@ using std::shared_ptr;
 using std::string;
 using std::weak_ptr;
 
+#pragma region Implementing more states
 /* Java @ https://github.com/bethrobson/Head-First-Design-Patterns/tree/master/src/headfirst/designpatterns/state/gumball
 public class HasQuarterState implements State {
-    GumballMachine gumballMachine;
-    public HasQuarterState(GumballMachine gumballMachine) { this.gumballMachine = gumballMachine; }
-    public void insertQuarter() { System.out.println("You can't insert another quarter"); }
-    public void ejectQuarter() {
-        System.out.println("Quarter returned");
-        gumballMachine.setState(gumballMachine.getNoQuarterState());
-    }
-    public void turnCrank() {
-        System.out.println("You turned...");
-        gumballMachine.setState(gumballMachine.getSoldState());
-    }
-    public void dispense() { System.out.println("No gumball dispensed"); }
-    public String toString() { return "waiting for turn of crank"; }
+	GumballMachine gumballMachine;
+	public HasQuarterState(GumballMachine gumballMachine) { this.gumballMachine = gumballMachine; }
+	public void insertQuarter() { System.out.println("You can't insert another quarter"); }
+	public void ejectQuarter() {
+		System.out.println("Quarter returned");
+		gumballMachine.setState(gumballMachine.getNoQuarterState());
+	}
+	public void turnCrank() {
+		System.out.println("You turned...");
+		gumballMachine.setState(gumballMachine.getSoldState());
+	}
+	public void dispense() { System.out.println("No gumball dispensed"); }
+	public String toString() { return "waiting for turn of crank"; }
 }
 */
-
 class HasQuarterState : public State {
-    weak_ptr<GumballMachine> gumballMachine;
+	weak_ptr<GumballMachine> gumballMachine;
 public:
-    HasQuarterState(shared_ptr<GumballMachine> gumballMachine) : gumballMachine(gumballMachine) {}
-    void insertQuarter() override { cout << "You can't insert another quarter\n"; }
+	HasQuarterState(shared_ptr<GumballMachine> gumballMachine) : gumballMachine(gumballMachine) {}
+	void insertQuarter() override { cout << "You can't insert another quarter\n"; }
 
 	void ejectQuarter() override {
 		if (auto machine = gumballMachine.lock()) {
@@ -39,14 +39,15 @@ public:
 			machine->setState(machine->getNoQuarterState());
 		}
 	}
-    
+
 	void turnCrank() override {
 		if (auto machine = gumballMachine.lock()) {
 			cout << "You turned...\n";
 			machine->setState(machine->getSoldState());
 		}
-    }
-    
+	}
+
 	void dispense() override { cout << "No gumball dispensed\n"; }
-    string toString() override { return "waiting for turn of crank"; }
+	string toString() override { return "waiting for turn of crank"; }
 };
+#pragma endregion //Implementing more states
--- C++/Examples/Head First Design Patterns/10 - 04 Implementing our State classes/StateSold.h
+++ C++/Examples/Head First Design Patterns/10 - 04 Implementing our State classes/StateSold.h
@@ -7,43 +7,44 @@ using std::cout;
 using std::shared_ptr;
 using std::string;
 
+#pragma region Implementing more states
 /* Java @ https://github.com/bethrobson/Head-First-Design-Patterns/tree/master/src/headfirst/designpatterns/state/gumball
 public class SoldState implements State {
-    GumballMachine gumballMachine;
-    public SoldState(GumballMachine gumballMachine) { this.gumballMachine = gumballMachine; }
-    public void insertQuarter() { System.out.println("Please wait, we're already giving you a gumball"); }
-    public void ejectQuarter() { System.out.println("Sorry, you already turned the crank"); }
-    public void turnCrank() { System.out.println("Turning twice doesn't get you another gumball!"); }
-    public void dispense() {
-        gumballMachine.releaseBall();
-        if (gumballMachine.getCount() > 0) gumballMachine.setState(gumballMachine.getNoQuarterState());
-        else {
-            System.out.println("Oops, out of gumballs!");
-            gumballMachine.setState(gumballMachine.getSoldOutState());
-        }
-    }
-    public String toString() { return "dispensing a gumball"; }
+	GumballMachine gumballMachine;
+	public SoldState(GumballMachine gumballMachine) { this.gumballMachine = gumballMachine; }
+	public void insertQuarter() { System.out.println("Please wait, we're already giving you a gumball"); }
+	public void ejectQuarter() { System.out.println("Sorry, you already turned the crank"); }
+	public void turnCrank() { System.out.println("Turning twice doesn't get you another gumball!"); }
+	public void dispense() {
+		gumballMachine.releaseBall();
+		if (gumballMachine.getCount() > 0) gumballMachine.setState(gumballMachine.getNoQuarterState());
+		else {
+			System.out.println("Oops, out of gumballs!");
+			gumballMachine.setState(gumballMachine.getSoldOutState());
+		}
+	}
+	public String toString() { return "dispensing a gumball"; }
 }
 */
-
 class SoldState : public State {
-    weak_ptr<GumballMachine> gumballMachine;
+	weak_ptr<GumballMachine> gumballMachine;
 public:
-    SoldState(shared_ptr<GumballMachine> gumballMachine) : gumballMachine(gumballMachine) {}
-    void insertQuarter() override { cout << "Please wait, we're already giving you a gumball\n"; }
-    void ejectQuarter() override { cout << "Sorry, you already turned the crank\n"; }
-    void turnCrank() override { cout << "Turning twice doesn't get you another gumball!\n"; }
+	SoldState(shared_ptr<GumballMachine> gumballMachine) : gumballMachine(gumballMachine) {}
+	void insertQuarter() override { cout << "Please wait, we're already giving you a gumball\n"; }
+	void ejectQuarter() override { cout << "Sorry, you already turned the crank\n"; }
+	void turnCrank() override { cout << "Turning twice doesn't get you another gumball!\n"; }
 
-    void dispense() override {
-        if (auto machine = gumballMachine.lock()) {
-            machine->releaseBall();
-            if (machine->getCount() > 0) 
-                machine->setState(machine->getNoQuarterState());
-            else {
-                cout << "Oops, out of gumballs!\n";
-                machine->setState(machine->getSoldOutState());
-            }
-        }
-    }
-    string toString() override { return "dispensing a gumball"; }
+	void dispense() override {
+		if (auto machine = gumballMachine.lock()) {
+			machine->releaseBall();
+			if (machine->getCount() > 0)
+				machine->setState(machine->getNoQuarterState());
+			else {
+				cout << "Oops, out of gumballs!\n";
+				machine->setState(machine->getSoldOutState());
+			}
+		}
+	}
+	string toString() override { return "dispensing a gumball"; }
 };
+#pragma endregion //Implementing more states
