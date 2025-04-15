1e852d3092a8464747ec0f7b4f1e0b27c798b5a9
apkretov <apkretov@hotmail.com>
2025-04-15 13:33:16 +0300

    Formatting

--- C++/Examples/Head First Design Patterns/11 - 01 Coding the Monitor/GumballMonitor.h
+++ C++/Examples/Head First Design Patterns/11 - 01 Coding the Monitor/GumballMonitor.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <iostream>
-#include <string>
+//#include <string>
 #include <memory>
 #include "GumballMachine.h"
 using std::cout;
--- C++/Examples/Head First Design Patterns/11 - 01 Coding the Monitor/State.h
+++ C++/Examples/Head First Design Patterns/11 - 01 Coding the Monitor/State.h
@@ -11,7 +11,6 @@ public interface State {
     public void dispense();
 }
 */
-
 class State {
 public:
     virtual ~State() = default;
