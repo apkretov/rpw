b527d4cd4209ade7ef6b118c6feee765b0362667
apkretov <apkretov@hotmail.com>
2025-04-16 17:38:42 +0300

    Stopping server

--- C++/Examples/Head First Design Patterns/11 - 02 The role of the remote proxy/main.cpp
+++ C++/Examples/Head First Design Patterns/11 - 02 The role of the remote proxy/main.cpp
@@ -2,39 +2,43 @@
 #include <boost/exception/diagnostic_information.hpp>
 #include <boost/exception/exception.hpp>
 #include "../../stdafx.h"
-//OFF #include "vld.h"
+#include "vld.h"
 #include "GumballMachine.h"
 #include "GumballMachineServer.h"
 #include "GumballMachineProxy.h"
 #include "GumballMonitor.h"
 
 #pragma region Trae
-void runServer() { // Server example
-    boost::asio::io_context io_context;
+void runServer(std::shared_ptr<boost::asio::io_context> io_context) { // Server example
     auto machine = GumballMachine::create("Seattle", 5);
-    GumballMachineServer server(io_context, 12345, machine);
-    io_context.run();
+    GumballMachineServer server(*io_context, 12345, machine);
+    io_context->run();
 }
 
 void runClient() { // Client example
     boost::asio::io_context io_context;
     auto proxy = std::make_shared<GumballMachineProxy>(io_context, "localhost", 12345);
     GumballMonitor monitor(proxy);
-	try {
-		monitor.report();
-	}
-	catch (const boost::exception &e) {
-		std::cerr << "Exception: " << boost::diagnostic_information(e) << "\n";
-	}
+    try {
+        monitor.report();
+    }
+    catch (const boost::exception &e) {
+        std::cerr << "Exception: " << boost::diagnostic_information(e) << "\n";
+    }
 }
 
 int main() {
-	print_file_line();
+    print_file_line();
 
-	std::thread server_thread(runServer); // Run server in a separate thread
-	std::this_thread::sleep_for(std::chrono::milliseconds(100)); // Give the server a moment to start
-	runClient(); // Run client
-	server_thread.join(); // Cleanup
+    auto io_context = std::make_shared<boost::asio::io_context>();
+    std::thread server_thread([io_context] { runServer(io_context); });
+    
+    std::this_thread::sleep_for(std::chrono::milliseconds(100)); // Give the server a moment to start
+    runClient(); // Run client
+    
+    io_context->stop(); // Signal the server to stop and cleanup
+    server_thread.join();
+	std::cout << '\n';
 
     return 0;
 }
