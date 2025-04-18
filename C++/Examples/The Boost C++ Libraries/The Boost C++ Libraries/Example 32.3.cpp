580b99f41fed5cb683b1b671839197aa77eae75f
apkretov <apkretov@hotmail.com>
2025-04-18 13:29:58 +0300

    Formatting

--- C++/Examples/The Boost C++ Libraries/The Boost C++ Libraries/Example 32.3.cpp
+++ C++/Examples/The Boost C++ Libraries/The Boost C++ Libraries/Example 32.3.cpp
@@ -19,9 +19,9 @@ void example_32_3() {
 
 	io_service ioservice;
 	steady_timer timer1{ioservice, seconds{3}};
-	timer1.async_wait([](const error_code &ec) { cout << "3 sec\n"; });
+	timer1.async_wait([](const error_code &ec) { cout << "1: 3 sec\n"; });
 	steady_timer timer2{ioservice, seconds{3}};
-	timer2.async_wait([](const error_code &ec) { cout << "3 sec\n"; });
+	timer2.async_wait([](const error_code &ec) { cout << "2: 3 sec\n"; });
 	thread thread1{[&ioservice]() { ioservice.run(); }};
 	thread thread2{[&ioservice]() { ioservice.run(); }};
 	thread1.join();
@@ -95,8 +95,10 @@ int main() {
 #else //MINE
 	example_32_3_mine();
 #endif //MINE
+#if 0
 	cout << "\nMINE:\n";
 	mine();
+#endif //0
 
 	return 0;
 }
--- C++/Examples/The Boost C++ Libraries/The Boost C++ Libraries/Example 32.4.cpp
+++ C++/Examples/The Boost C++ Libraries/The Boost C++ Libraries/Example 32.4.cpp
@@ -1,4 +1,5 @@
 #if 1
+
 #include <boost/asio/io_service.hpp>
 #include <boost/asio/steady_timer.hpp>
 #include <chrono>
@@ -7,22 +8,24 @@
 #include "../../stdafx.h"
 
 int main() {
-#pragma region aliases
+#pragma region Aliases
     using io_service =    boost::asio::io_service;
     using steady_timer =  boost::asio::steady_timer;
     using error_code =    boost::system::error_code;
     using seconds =       std::chrono::seconds;
     using                 std::cout;
     using thread =        std::thread;
-#pragma endregion //aliases
+#pragma endregion //Aliases
     print_file_line();
 
-    io_service ioservice1;
+	cout << "Starting the timers...\n"; //MINE
+
+	io_service ioservice1;
     io_service ioservice2;
     steady_timer timer1{ioservice1, seconds{3}};
-    timer1.async_wait([](const error_code& ec) { cout << "3 sec\n"; });
+    timer1.async_wait([](const error_code& ec) { cout << "1: 3 sec\n"; });
     steady_timer timer2{ioservice2, seconds{3}};
-    timer2.async_wait([](const error_code& ec) { cout << "3 sec\n"; });
+    timer2.async_wait([](const error_code& ec) { cout << "2: 3 sec\n"; });
     thread thread1{[&ioservice1]() { ioservice1.run(); }};
     thread thread2{[&ioservice2]() { ioservice2.run(); }};
     thread1.join();
