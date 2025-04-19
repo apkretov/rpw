983e68bf853ac0770fe44b0a9722ded93589399e
apkretov <apkretov@hotmail.com>
2025-04-19 15:33:56 +0300

    Formatting

--- C++/Examples/Head First Design Patterns/11 - 02 The role of the remote proxy/main.cpp
+++ C++/Examples/Head First Design Patterns/11 - 02 The role of the remote proxy/main.cpp
@@ -8,10 +8,10 @@
 #include "GumballMonitor.h"
 
 #pragma region Aliases
-using b_exception           = boost::exception;
 using io_context            = boost::asio::io_context;
+using b_exception           = boost::exception;
 using io_context_ptr        = std::shared_ptr<io_context>;
-using system_error         = boost::system::error_code;
+using system_error          = boost::system::error_code;
 using std::cerr;
 using std::cout;
 using std::string;
@@ -19,34 +19,37 @@ using std::thread;
 #pragma endregion
 
 #pragma region Trae
-void runServer(io_context_ptr io_context_) { // Server example
+void runServer(io_context_ptr io_context_ptr_) { // Server example
     auto machine = GumballMachine::create("Seattle", 5);
-    GumballMachineServer server(*io_context_, 12345, machine);
-    io_context_->run(); // This starts the event loop.
+    GumballMachineServer server(*io_context_ptr_, 12345, machine);
+    io_context_ptr_->run(); // This starts the event loop.
 }
 
 void runClient() { // Client example
+	using namespace boost;
+	using namespace std;
     io_context io_context_;
-    auto proxy = std::make_shared<GumballMachineProxy>(io_context_, "localhost", 12345);
-    GumballMonitor monitor(proxy);
+    auto GumballMachineProxy_ptr = make_shared<GumballMachineProxy>(io_context_, "localhost", 12345);
+    GumballMonitor monitor(GumballMachineProxy_ptr);
     try {
         monitor.report();
     }
     catch (const b_exception &e) {  // This line needs to be updated too
-        cerr << "Exception: " << boost::diagnostic_information(e) << "\n";
+        cerr << "Exception: " << diagnostic_information(e) << "\n";
     }
 }
 
 int main() {
+	using namespace std;
     print_file_line();
 
-    auto io_context_ = std::make_shared<io_context>();
-    thread server_thread([io_context_]() { runServer(io_context_); });
+    auto io_context_ptr = make_shared<io_context>();
+    thread server_thread([io_context_ptr]() { runServer(io_context_ptr); });
     
-    std::this_thread::sleep_for(std::chrono::milliseconds(100)); // Give the server a moment to start
+    this_thread::sleep_for(chrono::milliseconds(100)); // Give the server a moment to start
     runClient(); // Run client
     
-    io_context_->stop(); // Signal the server to stop and cleanup
+    io_context_ptr->stop(); // Signal the server to stop and cleanup
     server_thread.join();
     cout << '\n';
 
