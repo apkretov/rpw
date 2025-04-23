d0d9ad9f0429543ab133ab572e7d3e027d2dc333
apkretov <apkretov@hotmail.com>
2025-04-23 19:30:42 +0300

    vld.h

--- C++/Examples/Head First Design Patterns/11 - 05 Using the Java API's Proxy to create a protection proxy/main.cpp
+++ C++/Examples/Head First Design Patterns/11 - 05 Using the Java API's Proxy to create a protection proxy/main.cpp
@@ -1,7 +1,12 @@
+#include "../../stdafx.h"
+#include "vld.h"
 #include "MatchMakingTestDrive.h"
 
 int main() {
+	print_file_line();
+
     MatchMakingTestDrive test;
     test.drive();
+
     return 0;
 }
