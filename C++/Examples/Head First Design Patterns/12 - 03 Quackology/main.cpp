b1995b3f17e6dcb563b36a47609f84283fa834c9
apkretov <apkretov@hotmail.com>
2025-04-25 19:34:51 +0300

    My simulator

--- C++/Examples/Head First Design Patterns/12 - 03 Quackology/main.cpp
+++ C++/Examples/Head First Design Patterns/12 - 03 Quackology/main.cpp
@@ -1,4 +1,6 @@
+#include <array>
 #include <memory>
+#include "../../stdafx.h"
 #include "DuckMallard.h"
 #include "DuckRedhead.h"
 #include "DuckCall.h"
@@ -6,63 +8,27 @@
 #include "GooseAdapter.h"
 #include "QuackCounter.h"
 
-#pragma region We need a goose adapter
-/* Java @ https://github.com/bethrobson/Head-First-Design-Patterns/tree/master/src/headfirst/designpatterns/combining/adapter
-package headfirst.designpatterns.combining.adapter;
-
-public class DuckSimulator {
-	public static void main(String[] args) {
-		DuckSimulator simulator = new DuckSimulator();
-		simulator.simulate();
-	}
-
-	void simulate() {
-		Quackable mallardDuck = new MallardDuck();
-		Quackable redheadDuck = new RedheadDuck();
-		Quackable duckCall = new DuckCall();
-		Quackable rubberDuck = new RubberDuck();
-		Quackable gooseDuck = new GooseAdapter(new Goose());
- 
-		System.out.println("\nDuck Simulator: With Goose Adapter");
- 
-		simulate(mallardDuck);
-		simulate(redheadDuck);
-		simulate(duckCall);
-		simulate(rubberDuck);
-		simulate(gooseDuck);
-	}
- 
-	void simulate(Quackable duck) {
-		duck.quack();
-	}
-}
-*/
+#pragma region MINE
 class DuckSimulator {
 public:
-	void simulate(Quackable &duck) { duck.quack(); }
+	void simulate(QuackCounter &duck) { duck.quack(); }
 
 	void simulate() {
-		using QuackablePtr = std::unique_ptr<Quackable>;
+		MallardDuck mallardDuck;
+		GooseAdapter gooseDuck(Goose{});
+		std::array<QuackCounter, 2> arr{QuackCounter{mallardDuck}, QuackCounter{gooseDuck}};
 
-		QuackablePtr mallardDuck = std::make_unique<MallardDuck>();
-		QuackablePtr redheadDuck = std::make_unique<RedheadDuck>();
-		QuackablePtr duckCall = std::make_unique<DuckCall>();
-		QuackablePtr rubberDuck = std::make_unique<RubberDuck>();
-		QuackablePtr gooseDuck = std::make_unique<GooseAdapter>(Goose());
-
-		cout << "\nDuck Simulator: With Goose Adapter\n";
-
-		simulate(*mallardDuck);
-		simulate(*redheadDuck);
-		simulate(*duckCall);
-		simulate(*rubberDuck);
-		simulate(*gooseDuck);
+		for (auto &item : arr)
+			simulate(item);
+		std::cout << AS_KV(arr.at(0).getQuacks()) << '\n';
 	}
 };
 
 int main() {
+	print_file_line();
+
 	DuckSimulator simulator;
 	simulator.simulate();
 	return 0;
 }
-#pragma endregion //We need a goose adapter
+#pragma endregion //MINE
