25ac4b27c0c9e0015c1923911d485d3ac085db51
apkretov <apkretov@hotmail.com>
2025-04-29 12:57:43 +0300

    FactoryAbstractGoose.h

--- C++/Examples/Head First Design Patterns/12 - 05 Sharpen your pencil/FactoryAbstractGoose.h
+++ C++/Examples/Head First Design Patterns/12 - 05 Sharpen your pencil/FactoryAbstractGoose.h
@@ -1,12 +1,11 @@
 #pragma once
 
+#include "Honkable.h"
+
 #pragma region Sharpen your pencil
 class AbstractGooseFactory {
 public:
 	virtual ~AbstractGooseFactory() = default;
-	virtual QuackablePtr createMallardDuck() = 0;
-	virtual QuackablePtr createRedheadDuck() = 0;
-	virtual QuackablePtr createDuckCall() = 0;
-	virtual QuackablePtr createRubberDuck() = 0;
+	virtual HonkablePtr createGoose() = 0;
 };
 #pragma endregion //Sharpen your pencil
