522aa2f85469ea1aec558a44bf46354d6d130a9c
apkretov <apkretov@hotmail.com>
2025-04-29 13:37:12 +0300

    FactoryGoose.h

--- C++/Examples/Head First Design Patterns/12 - 05 Sharpen your pencil/FactoryGoose.h
+++ C++/Examples/Head First Design Patterns/12 - 05 Sharpen your pencil/FactoryGoose.h
@@ -2,10 +2,11 @@
 
 #include "FactoryAbstractGoose.h"
 #include "Goose.h"
+#include "GooseAdapter.h"
 
 #pragma region Sharpen your pencil
 class GooseFactory : public AbstractGooseFactory {
 public:
-	//HonkablePtr createGoose() override { return std::make_unique<Goose>(); }
+	QuackablePtr createGoose() override { return std::make_unique<GooseAdapter>(Goose{}); }
 };
 #pragma endregion //Sharpen your pencil
