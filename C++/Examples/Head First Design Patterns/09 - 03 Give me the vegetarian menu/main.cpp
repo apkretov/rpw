91b9120d75e8fb4d9a7fb5d14b7a01a87d627515
apkretov <apkretov@hotmail.com>
2025-05-01 17:24:36 +0300

    Java code corrected

--- C++/Examples/Head First Design Patterns/09 - 03 Give me the vegetarian menu/main.cpp
+++ C++/Examples/Head First Design Patterns/09 - 03 Give me the vegetarian menu/main.cpp
@@ -5,44 +5,120 @@
 #include <memory>
 using std::make_shared;
 
-/* Java @https://github.com/bethrobson/Head-First-Design-Patterns/tree/master/src/headfirst/designpatterns/composite/menuiterator
+/* Java @ https://github.com/bethrobson/Head-First-Design-Patterns/tree/master/src/headfirst/designpatterns/composite/menuiterator
+package headfirst.designpatterns.composite.menuiterator;
+
 public class MenuTestDrive {
-    public static void main(String args[]) {
-        MenuComponent pancakeHouseMenu = new Menu("PANCAKE HOUSE MENU", "Breakfast");
-        MenuComponent dinerMenu = new Menu("DINER MENU", "Lunch");
-        MenuComponent cafeMenu = new Menu("CAFE MENU", "Dinner");
-        MenuComponent dessertMenu = new Menu("DESSERT MENU", "Dessert of course!");
-  
-        MenuComponent allMenus = new Menu("ALL MENUS", "All menus combined");
-  
-        allMenus.add(pancakeHouseMenu);
-        allMenus.add(dinerMenu);
-        allMenus.add(cafeMenu);
-  
-        pancakeHouseMenu.add(make_shared<MenuItem>("K&B's Pancake Breakfast", "Pancakes with scrambled eggs", true, 2.99));
-        pancakeHouseMenu.add(make_shared<MenuItem>("Regular Pancake Breakfast", "Pancakes with fried eggs, sausage", false, 2.99));
-        pancakeHouseMenu.add(make_shared<MenuItem>("Blueberry Pancakes", "Pancakes made with fresh blueberries", true, 3.49));
-        pancakeHouseMenu.add(make_shared<MenuItem>("Waffles", "Waffles with your choice of blueberries or strawberries", true, 3.59));
+	public static void main(String args[]) {
 
-        dinerMenu.add(make_shared<MenuItem>("Vegetarian BLT", "(Fakin') Bacon with lettuce & tomato on whole wheat", true, 2.99));
-        dinerMenu.add(make_shared<MenuItem>("BLT", "Bacon with lettuce & tomato on whole wheat", false, 2.99));
-        dinerMenu.add(make_shared<MenuItem>("Soup of the day", "A bowl of the soup of the day, with a side of potato salad", false, 3.29));
-        dinerMenu.add(make_shared<MenuItem>("Hot Dog", "A hot dog, with saurkraut, relish, onions, topped with cheese", false, 3.05));
-        dinerMenu.add(make_shared<MenuItem>("Steamed Veggies and Brown Rice", "Steamed vegetables over brown rice", true, 3.99));
-        dinerMenu.add(make_shared<MenuItem>("Pasta", "Spaghetti with marinara sauce, and a slice of sourdough bread", true, 3.89));
-        dinerMenu.add(dessertMenu);
+		MenuComponent pancakeHouseMenu =
+			new Menu("PANCAKE HOUSE MENU", "Breakfast");
+		MenuComponent dinerMenu =
+			new Menu("DINER MENU", "Lunch");
+		MenuComponent cafeMenu =
+			new Menu("CAFE MENU", "Dinner");
+		MenuComponent dessertMenu =
+			new Menu("DESSERT MENU", "Dessert of course!");
 
-        dessertMenu.add(make_shared<MenuItem>("Apple Pie", "Apple pie with a flakey crust, topped with vanilla icecream", true, 1.59));
-        dessertMenu.add(make_shared<MenuItem>("Cheesecake", "Creamy New York cheesecake, with a chocolate graham crust", true, 1.99));
-        dessertMenu.add(make_shared<MenuItem>("Sorbet", "A scoop of raspberry and a scoop of lime", true, 1.89));
+		MenuComponent allMenus = new Menu("ALL MENUS", "All menus combined");
 
-        cafeMenu.add(make_shared<MenuItem>("Veggie Burger and Air Fries", "Veggie burger on a whole wheat bun, lettuce, tomato, and fries", true, 3.99));
-        cafeMenu.add(make_shared<MenuItem>("Soup of the Day", "A cup of the soup of the day, with a side salad", false, 3.69));
-        cafeMenu.add(make_shared<MenuItem>("Burrito", "A large burrito, with whole pinto beans, salsa, guacamole", true, 4.29));
+		allMenus.add(pancakeHouseMenu);
+		allMenus.add(dinerMenu);
+		allMenus.add(cafeMenu);
 
-        Waitress waitress = new Waitress(allMenus);
-        waitress.printMenu();
-    }
+		pancakeHouseMenu.add(new MenuItem(
+			"K&B's Pancake Breakfast",
+			"Pancakes with scrambled eggs and toast",
+			true,
+			2.99));
+		pancakeHouseMenu.add(new MenuItem(
+			"Regular Pancake Breakfast",
+			"Pancakes with fried eggs, sausage",
+			false,
+			2.99));
+		pancakeHouseMenu.add(new MenuItem(
+			"Blueberry Pancakes",
+			"Pancakes made with fresh blueberries and blueberry syrup",
+			true,
+			3.49));
+		pancakeHouseMenu.add(new MenuItem(
+			"Waffles",
+			"Waffles with your choice of blueberries or strawberries",
+			true,
+			3.59));
+
+		dinerMenu.add(new MenuItem(
+			"Vegetarian BLT",
+			"(Fakin') Bacon with lettuce & tomato on whole wheat",
+			true,
+			2.99));
+		dinerMenu.add(new MenuItem(
+			"BLT",
+			"Bacon with lettuce & tomato on whole wheat",
+			false,
+			2.99));
+		dinerMenu.add(new MenuItem(
+			"Soup of the day",
+			"A bowl of the soup of the day, with a side of potato salad",
+			false,
+			3.29));
+		dinerMenu.add(new MenuItem(
+			"Hot Dog",
+			"A hot dog, with saurkraut, relish, onions, topped with cheese",
+			false,
+			3.05));
+		dinerMenu.add(new MenuItem(
+			"Steamed Veggies and Brown Rice",
+			"A medly of steamed vegetables over brown rice",
+			true,
+			3.99));
+
+		dinerMenu.add(new MenuItem(
+			"Pasta",
+			"Spaghetti with marinara sauce, and a slice of sourdough bread",
+			true,
+			3.89));
+
+		dinerMenu.add(dessertMenu);
+
+		dessertMenu.add(new MenuItem(
+			"Apple Pie",
+			"Apple pie with a flakey crust, topped with vanilla icecream",
+			true,
+			1.59));
+		dessertMenu.add(new MenuItem(
+			"Cheesecake",
+			"Creamy New York cheesecake, with a chocolate graham crust",
+			true,
+			1.99));
+		dessertMenu.add(new MenuItem(
+			"Sorbet",
+			"A scoop of raspberry and a scoop of lime",
+			true,
+			1.89));
+
+		cafeMenu.add(new MenuItem(
+			"Veggie Burger and Air Fries",
+			"Veggie burger on a whole wheat bun, lettuce, tomato, and fries",
+			true,
+			3.99));
+		cafeMenu.add(new MenuItem(
+			"Soup of the day",
+			"A cup of the soup of the day, with a side salad",
+			false,
+			3.69));
+		cafeMenu.add(new MenuItem(
+			"Burrito",
+			"A large burrito, with whole pinto beans, salsa, guacamole",
+			true,
+			4.29));
+
+		Waitress waitress = new Waitress(allMenus);
+
+		waitress.printVegetarianMenu();
+		//waitress.printMenu();
+
+	}
 }
 */
 int main() {
