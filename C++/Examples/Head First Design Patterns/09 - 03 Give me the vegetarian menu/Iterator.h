967b3fe5e4755a88c52a5338ab810379bad37550
apkretov <apkretov@hotmail.com>
2025-05-04 20:29:30 +0300

    get in Iterator.h

--- C++/Examples/Head First Design Patterns/09 - 03 Give me the vegetarian menu/Iterator.h
+++ C++/Examples/Head First Design Patterns/09 - 03 Give me the vegetarian menu/Iterator.h
@@ -6,7 +6,8 @@ class Iterator {
 public:
     virtual ~Iterator() = default;
     
-	virtual T& next() = 0;
+	virtual T get() const = 0;
+	virtual T &next() = 0;
     virtual bool hasNext() const = 0;
 };
 #pragma endregion //Trae
