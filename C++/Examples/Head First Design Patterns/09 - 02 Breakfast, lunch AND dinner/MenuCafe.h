bd710e5d127eba6cd328af53707615badf781e2c
apkretov <apkretov@hotmail.com>
2025-05-05 23:20:41 +0300

    MenuCafe.h

---
+++ C++/Examples/Head First Design Patterns/09 - 02 Breakfast, lunch AND dinner/MenuCafe.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "Menu.h"
+#include <unordered_map>
+
+/* Java @ https://github.com/bethrobson/Head-First-Design-Patterns/tree/master/src/headfirst/designpatterns/iterator/dinermergercafe
+public class CafeMenu implements Menu {
+	HashMap<String, MenuItem> menuItems = new HashMap<String, MenuItem>();
+
+	public CafeMenu() {
+		addItem("Veggie Burger and Air Fries", "Veggie burger on a whole wheat bun, lettuce, tomato, and fries", true, 3.99);
+		addItem("Soup of the day", "A cup of the soup of the day, with a side salad", false, 3.69);
+		addItem("Burrito", "A large burrito, with whole pinto beans, salsa, guacamole", true, 4.29);
+	}
+}
+*/
+class CafeMenu : public Menu {
+private:
+	std::unordered_map<std::string, MenuItem> menuItems;
+
+public:
+	CafeMenu() {
+		addItem("Veggie Burger and Air Fries", "Veggie burger on a whole wheat bun, lettuce, tomato, and fries", true, 3.99);
+		addItem("Soup of the day", "A cup of the soup of the day, with a side salad", false, 3.69);
+		addItem("Burrito", "A large burrito, with whole pinto beans, salsa, guacamole", true, 4.29);
+	}
+
+	void addItem(const std::string &name, const std::string &description, bool vegetarian, double price) {
+		MenuItem menuItem(name, description, vegetarian, price);
+		menuItems[name] = menuItem;
+	}
+
+	std::shared_ptr<Iterator<MenuItem>> createIterator() override;
+};
