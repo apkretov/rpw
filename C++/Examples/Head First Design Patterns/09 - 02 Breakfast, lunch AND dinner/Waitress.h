5fc2195428d364225d2f04fe5d221f4944ba3ec1
apkretov <apkretov@hotmail.com>
2025-05-05 20:19:42 +0300

    Waitress.h

--- C++/Examples/Head First Design Patterns/09 - 02 Breakfast, lunch AND dinner/Waitress.h
+++ C++/Examples/Head First Design Patterns/09 - 02 Breakfast, lunch AND dinner/Waitress.h
@@ -1,61 +1,153 @@
 #pragma once
+
 #include <iostream>
 #include <memory>
-#include "MenuComponent.h"
+#include "Menu.h"
+#include "MenuItem.h"
 
-/* Java @https://github.com/bethrobson/Head-First-Design-Patterns/tree/master/src/headfirst/designpatterns/composite/menuiterator
-package headfirst.designpatterns.composite.menuiterator;
+/* Java @ https://github.com/bethrobson/Head-First-Design-Patterns/tree/master/src/headfirst/designpatterns/iterator/dinermergercafe
+package headfirst.designpatterns.iterator.dinermergercafe;
 
 import java.util.Iterator;
-  
-public class Waitress {
-	MenuComponent allMenus;
- 
-	public Waitress(MenuComponent allMenus) {
-		this.allMenus = allMenus;
-	}
- 
-	public void printMenu() {
-		allMenus.print();
-	}
-  
-	public void printVegetarianMenu() {
-		Iterator<MenuComponent> iterator = allMenus.createIterator();
 
-		System.out.println("\nVEGETARIAN MENU\n----");
+public class Waitress {
+	Menu pancakeHouseMenu;
+	Menu dinerMenu;
+	Menu cafeMenu;
+
+	public Waitress(Menu pancakeHouseMenu, Menu dinerMenu, Menu cafeMenu) {
+		this.pancakeHouseMenu = pancakeHouseMenu;
+		this.dinerMenu = dinerMenu;
+		this.cafeMenu = cafeMenu;
+	}
+
+	public void printMenu() {
+		Iterator<MenuItem> pancakeIterator = pancakeHouseMenu.createIterator();
+		Iterator<MenuItem> dinerIterator = dinerMenu.createIterator();
+		Iterator<MenuItem> cafeIterator = cafeMenu.createIterator();
+
+		System.out.println("MENU\n----\nBREAKFAST");
+		printMenu(pancakeIterator);
+		System.out.println("\nLUNCH");
+		printMenu(dinerIterator);
+		System.out.println("\nDINNER");
+		printMenu(cafeIterator);
+	}
+
+	private void printMenu(Iterator<MenuItem> iterator) {
 		while (iterator.hasNext()) {
-			MenuComponent menuComponent = iterator.next();
-			try {
-				if (menuComponent.isVegetarian()) {
-					menuComponent.print();
-				}
-			} catch (UnsupportedOperationException e) {}
+			MenuItem menuItem = iterator.next();
+			System.out.print(menuItem.getName() + ", ");
+			System.out.print(menuItem.getPrice() + " -- ");
+			System.out.println(menuItem.getDescription());
 		}
+	}
+
+	public void printVegetarianMenu() {
+		System.out.println("\nVEGETARIAN MENU\n---------------");
+		printVegetarianMenu(pancakeHouseMenu.createIterator());
+		printVegetarianMenu(dinerMenu.createIterator());
+		printVegetarianMenu(cafeMenu.createIterator());
+	}
+
+	public boolean isItemVegetarian(String name) {
+		Iterator<MenuItem> pancakeIterator = pancakeHouseMenu.createIterator();
+		if (isVegetarian(name, pancakeIterator)) {
+			return true;
+		}
+		Iterator<MenuItem> dinerIterator = dinerMenu.createIterator();
+		if (isVegetarian(name, dinerIterator)) {
+			return true;
+		}
+		Iterator<MenuItem> cafeIterator = cafeMenu.createIterator();
+		if (isVegetarian(name, cafeIterator)) {
+			return true;
+		}
+		return false;
 	}
 }
 */
-class Waitress {
-    PtrMenucompont allMenus;
-public:
-    Waitress(PtrMenucompont allMenus) : allMenus(allMenus) {}
-    void printMenu() { allMenus->print(); }
 
-    void printVegetarianMenu() {
-        std::cout << "\nVEGETARIAN MENU\n----\n";
-#ifdef DEBUG
-		std::clog << "010 " << typeid(*allMenus).name() << ": " << allMenus->getName() << '\n'; //MINE  
-#endif //DEBUG
-		PtrIterMenucompont iterator = allMenus->createIterator();
-#ifdef DEBUG
-		std::clog << "050 " << typeid(iterator->current()).name() << ": " << iterator->current().getName() << '\n'; //MINE
-#endif //DEBUG
-        while (iterator->hasNext()) {
-			MenuComponent &menuComponent = iterator->next();
-#ifdef DEBUG
-			std::clog << "100 " << typeid(menuComponent).name() << ": " << menuComponent.getName() << '\n'; //MINE
-#endif //DEBUG
-            if (menuComponent.isVegetarian())
-                menuComponent.print();
+class Waitress {
+	std::shared_ptr<Menu> pancakeHouseMenu;
+	std::shared_ptr<Menu> dinerMenu;
+	std::shared_ptr<Menu> cafeMenu;
+
+public:
+	Waitress(std::shared_ptr<Menu> pancakeHouseMenu,
+		std::shared_ptr<Menu> dinerMenu,
+		std::shared_ptr<Menu> cafeMenu)
+		: pancakeHouseMenu(pancakeHouseMenu)
+		, dinerMenu(dinerMenu)
+		, cafeMenu(cafeMenu) {
+	}
+
+	void printMenu() {
+		auto pancakeIterator = pancakeHouseMenu->createIterator();
+		auto dinerIterator = dinerMenu->createIterator();
+		auto cafeIterator = cafeMenu->createIterator();
+
+		std::cout << "MENU\n----\nBREAKFAST\n";
+		printMenu(pancakeIterator);
+		std::cout << "\nLUNCH\n";
+		printMenu(dinerIterator);
+		std::cout << "\nDINNER\n";
+		printMenu(cafeIterator);
+	}
+
+private:
+	void printMenu(std::shared_ptr<Iterator<MenuItem>> iterator) {
+		while (iterator->hasNext()) {
+			auto &menuItem = iterator->next();
+			std::cout << menuItem.getName() << ", ";
+			std::cout << menuItem.getPrice() << " -- ";
+			std::cout << menuItem.getDescription() << "\n";
 		}
-    }
+	}
+
+public:
+	void printVegetarianMenu() {
+		std::cout << "\nVEGETARIAN MENU\n---------------\n";
+		printVegetarianMenu(pancakeHouseMenu->createIterator());
+		printVegetarianMenu(dinerMenu->createIterator());
+		printVegetarianMenu(cafeMenu->createIterator());
+	}
+
+	bool isItemVegetarian(const std::string &name) {
+		auto pancakeIterator = pancakeHouseMenu->createIterator();
+		if (isVegetarian(name, pancakeIterator)) {
+			return true;
+		}
+		auto dinerIterator = dinerMenu->createIterator();
+		if (isVegetarian(name, dinerIterator)) {
+			return true;
+		}
+		auto cafeIterator = cafeMenu->createIterator();
+		if (isVegetarian(name, cafeIterator)) {
+			return true;
+		}
+		return false;
+	}
+
+private:
+	void printVegetarianMenu(std::shared_ptr<Iterator<MenuItem>> iterator) {
+		while (iterator->hasNext()) {
+			auto &menuItem = iterator->next();
+			if (menuItem.isVegetarian()) {
+				std::cout << menuItem.getName() << ", ";
+				std::cout << menuItem.getPrice() << " -- ";
+				std::cout << menuItem.getDescription() << "\n";
+			}
+		}
+	}
+
+	bool isVegetarian(const std::string &name, std::shared_ptr<Iterator<MenuItem>> iterator) {
+		while (iterator->hasNext()) {
+			auto &menuItem = iterator->next();
+			if (menuItem.getName() == name) {
+				return menuItem.isVegetarian();
+			}
+		}
+		return false;
+	}
 };
