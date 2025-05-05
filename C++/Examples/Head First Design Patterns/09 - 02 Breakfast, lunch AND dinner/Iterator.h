79a7265f5c6bd632930c70880c42a08e5151c512
apkretov <apkretov@hotmail.com>
2025-05-05 20:39:19 +0300

    Iterator.h

--- C++/Examples/Head First Design Patterns/09 - 02 Breakfast, lunch AND dinner/Iterator.h
+++ C++/Examples/Head First Design Patterns/09 - 02 Breakfast, lunch AND dinner/Iterator.h
@@ -1,13 +1,17 @@
 #pragma once
 
-#pragma region Trae
-template <typename T>
+#pragma once
+
+/* Java code
+public interface Iterator<T> {
+	boolean hasNext();
+	T next();
+}
+*/
+template<typename T>
 class Iterator {
 public:
-    virtual ~Iterator() = default;
-    
-	virtual T &current() const = 0; //MINE
+	virtual ~Iterator() = default;
+	virtual bool hasNext() = 0;
 	virtual T &next() = 0;
-    virtual bool hasNext() const = 0;
 };
-#pragma endregion //Trae
