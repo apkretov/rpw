bdbb7ad840ccebd44b8c2a8ca94db33175ddc789
apkretov <apkretov@hotmail.com>
2025-05-09 16:56:37 +0300

    Flock.h

--- C++/Examples/Head First Design Patterns/12 - 08 First we need an Observable interface/Flock.h
+++ C++/Examples/Head First Design Patterns/12 - 08 First we need an Observable interface/Flock.h
@@ -4,30 +4,20 @@
 #include <vector>
 #include "Quackable.h"
 
-#pragma region Lets create a flock of ducks //Let's create a flock of ducks(well, actually a flock of Quackables)
-/* Java 
-public class Flock implements Quackable { // Remember, the composite needs to implement the same interface as the leaf elements. Our leaf elements are Quackables.
-	ArrayList quackers = new ArrayList(); // We're using an ArrayList inside each Flock to hold the Quackables that belong to the Flock.
-	public void add(Quackable quacker) { // The add() method adds a Quackable to the Flock.
-		quackers.add(quacker);
-	}
-	public void quack() {						 // Now for the quack() method - after all, the Flock is a Quackable too. The quack() method in Flock needs
-		Iterator iterator = quackers.iterator(); // to work over the entire Flock. Here we iterate through the ArrayList and call quack() on each element.
-		while (iterator.hasNext()) {
-			Quackable quacker = (Quackable)iterator.next();
-			quacker.quack();
-		}
-	}
-}
-*/
+#pragma region Integrate the helper Observable with the Quackable classes
 class Flock : public Quackable { // Remember, the composite needs to implement the same interface as the leaf elements. Our leaf elements are Quackables.
 	std::vector<PtrQuackable> quackers; // We're using an ArrayList inside each Flock to hold the Quackables that belong to the Flock.
+	Observable observable;
 public:
+	Flock() : observable(*this) {}
 	void add(PtrQuackable quacker) { quackers.push_back(std::move(quacker)); } // The add() method adds a Quackable to the Flock.
 
 	void quack() override { // Now for the quack() method - after all, the Flock is a Quackable too. The quack() method in Flock needs to work over the entire Flock. Here we iterate through the ArrayList and call quack() on each element.
 		for (auto const &quacker : quackers) // There it is! The Iterator Pattern at work!
 			quacker->quack();
 	}
+
+	void registerObserver(PtrObserver observer) override { observable.registerObserver(observer); }
+	void notifyObservers() override { observable.notifyObservers(); }
 };
-#pragma endregion //Lets create a flock of ducks
+#pragma endregion //Integrate the helper Observable with the Quackable classes
