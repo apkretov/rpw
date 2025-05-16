16d2c34b1f18228f506a356f8d95668c8b01c2ea
apkretov <apkretov@hotmail.com>
2025-05-16 21:52:43 +0300

    Removed Heart* files

--- C++/Examples/Head First Design Patterns/12 - 09 Let’s check out the BeatModelInterface/main.cpp
+++ C++/Examples/Head First Design Patterns/12 - 09 Let’s check out the BeatModelInterface/main.cpp
@@ -2,34 +2,21 @@
 
 #include <memory>
 #include "BeatModel.h"
-#include "HeartModel.h"
 #include "BeatController.h"
-#include "HeartController.h"
 
-#pragma region Now lets have a look at the concrete BeatModel class //Now let's have a look at the concrete BeatModel class
-/* Java @ https://github.com/bethrobson/Head-First-Design-Patterns/tree/master/src/headfirst/designpatterns/combined/djview
+#pragma region Putting it all together...
+/* Java
 public class DJTestDrive {
     public static void main (String[] args) {
         BeatModelInterface model = new BeatModel();
         ControllerInterface controller = new BeatController(model);
     }
 }
-
-public class HeartTestDrive {
-    public static void main (String[] args) {
-        HeartModel heartModel = new HeartModel();
-        ControllerInterface model = new HeartController(heartModel);
-    }
-}
 */
 
 int main() {
-    auto beatModel = std::make_shared<BeatModel>(); // Test the Beat Model
-    auto beatController = std::make_unique<BeatController>(beatModel);
-
-    auto heartModel = std::make_shared<HeartModel>(); // Test the Heart Model
-    auto heartController = std::make_unique<HeartController>(heartModel);
-
+	auto beatModel = std::make_shared<BeatModel>(); // Test the Beat Model
+	auto beatController = std::make_unique<BeatController>(beatModel);
     return 0;
 }
-#pragma endregion //Now lets have a look at the concrete BeatModel class
+#pragma endregion //Putting it all together...
