7de3b44a84a0ce055bc660c1f823c7a88dee80e2
apkretov <apkretov@hotmail.com>
2025-05-19 17:45:57 +0300

    main.cpp began

--- C++/Examples/Head First Design Patterns/12-09_Lets_check_out_the_BeatModelInterface/main.cpp
+++ C++/Examples/Head First Design Patterns/12-09_Lets_check_out_the_BeatModelInterface/main.cpp
@@ -1,11 +1,24 @@
 #include "widget.h"
 
 #include <QApplication>
+#include "BeatModel.h"
+#include "ControllerBeat.h"
 
-int main(int argc, char *argv[])
-{
+/* Java
+public class DJTestDrive {
+    public static void main (String[] args) {
+        BeatModelInterface model = new BeatModel();
+        ControllerInterface controller = new BeatController(model);
+    }
+}
+*/
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     Widget w;
     w.show();
+
+    BeatModel beatModel;
+    BeatController beatController{beatModel};
+
     return a.exec();
 }
