28e77f2f8cea37877e1cf04566cb98d2bade2b7d
apkretov <apkretov@hotmail.com>
2025-05-21 18:38:15 +0300

    main.cpp updated, MyClass.h corrected

--- C++/Examples/Foundations Of Qt/01-02_Inheriting_QObject_and_accepting_a_parent/MyClass.h
+++ C++/Examples/Foundations Of Qt/01-02_Inheriting_QObject_and_accepting_a_parent/MyClass.h
@@ -4,11 +4,11 @@
 #include <string>
 
 #pragma region Listing 1-2. Inheriting QObject and accepting a parent
-class MyClass {
+class MyClass : public QObject {
 using string = std::string;
     string m_text;
 public:
-    MyClass(const string& text, QObject *parent = 0) : m_text(text) {}
+    MyClass(const string& text, QObject *parent = 0) : m_text(text), QObject(parent) {}
     const string& text() const { return m_text; }
     void setText(const string& text) { m_text = text; }
     int getLengthOfText() const { return m_text.length(); }
--- C++/Examples/Foundations Of Qt/01-02_Inheriting_QObject_and_accepting_a_parent/main.cpp
+++ C++/Examples/Foundations Of Qt/01-02_Inheriting_QObject_and_accepting_a_parent/main.cpp
@@ -1,18 +1,34 @@
 #include <QCoreApplication>
+#include <QDebug>
 #include "../stdafx.h"
 #include "MyClass.h"
 
+#pragma region Listing 1-4. Dynamic memory with Qt
 int main(int argc, char *argv[]) {
-    QCoreApplication a(argc, argv);
+    // QCoreApplication a(argc, argv);
 
     print_file_line(); //MINE
 
-    MyClass myClass("test");
-    std::cout << "Text: " << myClass.text() << '\n';
-    std::cout << "Length of text: " << myClass.getLengthOfText() << '\n';
+    // MyClass myClass("test");
+    // std::cout << "Text: " << myClass.text() << '\n';
+    // std::cout << "Length of text: " << myClass.getLengthOfText() << '\n';
 
-    myClass.setText("new text");
-    std::cout << "Updated text: " << myClass.text() << '\n';
+    // myClass.setText("new text");
+    // std::cout << "Updated text: " << myClass.text() << '\n';
 
-    return a.exec();
+    // return a.exec();
+
+    QObject parent;
+    MyClass *a, *b, *c;
+
+    a = new MyClass( "foo", &parent );
+    b = new MyClass( "ba-a-ar", &parent );
+    c = new MyClass( "baz", &parent );
+
+    qDebug() << QString::fromStdString(a->text()) << " (" << a->getLengthOfText() << ")";
+    a->setText( b->text() );
+    qDebug() << QString::fromStdString(a->text()) << " (" << a->getLengthOfText() << ")";
+
+    return a->getLengthOfText() - c->getLengthOfText();
 }
+#pragma endregion //Listing 1-4. Dynamic memory with Qt
