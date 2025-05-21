d407ffd5762a0bd74dab20efc264948d4e6cd4cc
apkretov <apkretov@hotmail.com>
2025-05-21 18:44:46 +0300

    Formatting

--- C++/Examples/Foundations Of Qt/01-02_Inheriting_QObject_and_accepting_a_parent/main.cpp
+++ C++/Examples/Foundations Of Qt/01-02_Inheriting_QObject_and_accepting_a_parent/main.cpp
@@ -5,28 +5,17 @@
 
 #pragma region Listing 1-4. Dynamic memory with Qt
 int main(int argc, char *argv[]) {
-    // QCoreApplication a(argc, argv);
-
     print_file_line(); //MINE
-
-    // MyClass myClass("test");
-    // std::cout << "Text: " << myClass.text() << '\n';
-    // std::cout << "Length of text: " << myClass.getLengthOfText() << '\n';
-
-    // myClass.setText("new text");
-    // std::cout << "Updated text: " << myClass.text() << '\n';
-
-    // return a.exec();
 
     QObject parent;
     MyClass *a, *b, *c;
 
-    a = new MyClass( "foo", &parent );
-    b = new MyClass( "ba-a-ar", &parent );
-    c = new MyClass( "baz", &parent );
+    a = new MyClass("foo", &parent);
+    b = new MyClass("ba-a-ar", &parent);
+    c = new MyClass("baz", &parent);
 
     qDebug() << QString::fromStdString(a->text()) << " (" << a->getLengthOfText() << ")";
-    a->setText( b->text() );
+    a->setText(b->text());
     qDebug() << QString::fromStdString(a->text()) << " (" << a->getLengthOfText() << ")";
 
     return a->getLengthOfText() - c->getLengthOfText();
