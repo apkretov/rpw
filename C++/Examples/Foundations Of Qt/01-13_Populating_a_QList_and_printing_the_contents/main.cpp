72a03c45f9e803f7138797831271733814f05382
apkretov <apkretov@hotmail.com>
2025-05-22 20:50:38 +0300

    Listing 1-15

--- C++/Examples/Foundations Of Qt/01-13_Populating_a_QList_and_printing_the_contents/main.cpp
+++ C++/Examples/Foundations Of Qt/01-13_Populating_a_QList_and_printing_the_contents/main.cpp
@@ -17,16 +17,39 @@ void Listing_1_14() { //Listing 1-14. STL-style iterators and Java-style iterato
     while(javaIter.hasNext())
         qDebug() << javaIter.next();
 
+    qDebug("\n");
     QList<int>::const_iterator stlIter; //STL-style.
     for(stlIter = list.begin(); stlIter != list.end(); ++stlIter)
         qDebug() << (*stlIter);
+}
+
+void Listing_1_15() { //Listing 1-15. Modifying lists using iterators
+    QList<int> list;
+    list << 27 << 33 << 61 << 62;
+
+    QMutableListIterator<int> javaIter(list); //Java-style iterator
+    while(javaIter.hasNext()) {
+        int value = javaIter.next() + 1;
+        javaIter.setValue( value );
+        qDebug() << value;
+    }
+
+    qDebug("\n");
+    QList<int>::Iterator stlIter; //STL-style iterator
+    for(stlIter = list.begin(); stlIter != list.end(); ++stlIter) {
+        (*stlIter) = (*stlIter)*2;
+        qDebug() << (*stlIter);
+    }
 }
 
 int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
     
     Listing_1_13();
+    qDebug("\n");
     Listing_1_14();
+    qDebug("\n");
+    Listing_1_15();
 
     return a.exec();
 }
