9bc7efa357ac980b364fe39bf9c3980f4dd78800
apkretov <apkretov@hotmail.com>
2025-05-23 14:52:59 +0300

    Figure 2-24

---
+++ C++/Examples/Foundations Of Qt/02-02_The_header_file_for_the_ListDialog_class/editdialog.h
@@ -0,0 +1,22 @@
+#ifndef EDITDIALOG_H
+#define EDITDIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+class nameEdit;
+}
+
+class EditDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit EditDialog(QWidget *parent = nullptr);
+    ~EditDialog();
+
+private:
+    Ui::nameEdit *ui;
+};
+
+#endif // EDITDIALOG_H
