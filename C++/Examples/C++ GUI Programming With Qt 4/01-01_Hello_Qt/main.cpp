39e83633ea4ed5286599b20e15a96546d0218d02
apkretov <apkretov@hotmail.com>
2025-05-25 11:53:39 +0300

    Figure 1.2

--- C++/Examples/C++ GUI Programming With Qt 4/01-01_Hello_Qt/main.cpp
+++ C++/Examples/C++ GUI Programming With Qt 4/01-01_Hello_Qt/main.cpp
@@ -3,7 +3,11 @@
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
+#ifdef VER_1
     QLabel *label = new QLabel("Hello, Qt!");
+#else //Figure 1.2.
+    QLabel *label = new QLabel("<h2><i>Hello</i> " "<font color=red>Qt!</font></h2>");
+#endif //Figure 1.2
     label->show();
     return app.exec();
 }
