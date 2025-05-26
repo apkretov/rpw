d1a51de8f83007ce5d14276f474426da456f9c8f
apkretov <apkretov@hotmail.com>
2025-05-26 19:51:26 +0300

    main.cpp

--- C++/Examples/C++ GUI Programming With Qt 4/02-03_Rapid_Dialog_Design/main.cpp
+++ C++/Examples/C++ GUI Programming With Qt 4/02-03_Rapid_Dialog_Design/main.cpp
@@ -1,9 +1,12 @@
-#include "gotocelldialog.h"
 #include <QApplication>
+#include <QDialog>
+#include "ui_GoToCellDialog.h"
 
 int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
-    GoToCellDialog w;
-    w.show();
-    return a.exec();
+    QApplication app(argc, argv);
+    Ui::GoToCellDialog ui;
+    QDialog *dialog = new QDialog;
+    ui.setupUi(dialog);
+    dialog->show();
+    return app.exec();
 }
