8f271e575a040e161c2265acc75478584405b32c
apkretov <apkretov@hotmail.com>
2025-05-26 16:04:28 +0300

    main.cpp

--- C++/Examples/C++ GUI Programming With Qt 4/02-01_Subclassing_QDialog/main.cpp
+++ C++/Examples/C++ GUI Programming With Qt 4/02-01_Subclassing_QDialog/main.cpp
@@ -1,6 +1,9 @@
 #include <QApplication>
+#include "finddialog.h"
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+    FindDialog *dialog = new FindDialog;
+    dialog->show();
     return a.exec();
 }
