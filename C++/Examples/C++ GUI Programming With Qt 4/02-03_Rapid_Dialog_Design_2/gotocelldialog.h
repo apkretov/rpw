5425eecd9da4c7b977c266cd00a1089fa62fe29e
apkretov <apkretov@hotmail.com>
2025-05-27 15:16:51 +0300

    gotocelldialog.h

--- C++/Examples/C++ GUI Programming With Qt 4/02-03_Rapid_Dialog_Design_2/gotocelldialog.h
+++ C++/Examples/C++ GUI Programming With Qt 4/02-03_Rapid_Dialog_Design_2/gotocelldialog.h
@@ -1,4 +1,4 @@
-#ifndef QT
+#ifdef QT
 #ifndef GOTOCELLDIALOG_H
 #define GOTOCELLDIALOG_H
 
@@ -23,5 +23,15 @@ public:
 #ifndef GOTOCELLDIALOG_H
 #define GOTOCELLDIALOG_H
 
+#include <QDialog>
+#include "ui_gotocelldialog.h"
+
+class GoToCellDialog : public QDialog, Ui::GoToCellDialog {
+    Q_OBJECT
+public:
+    GoToCellDialog(QWidget *parent = 0);
+private slots:
+    void on_lineEdit_textChanged();
+};
 #endif // GOTOCELLDIALOG_H
 #endif //ORIG
