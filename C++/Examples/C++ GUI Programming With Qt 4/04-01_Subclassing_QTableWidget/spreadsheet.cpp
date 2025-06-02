753a2797e5c35d57b0c4dd09531628f9e7598764
apkretov <apkretov@hotmail.com>
2025-06-02 18:52:23 +0300

    spreadsheet.cpp

--- C++/Examples/C++ GUI Programming With Qt 4/04-01_Subclassing_QTableWidget/spreadsheet.cpp
+++ C++/Examples/C++ GUI Programming With Qt 4/04-01_Subclassing_QTableWidget/spreadsheet.cpp
@@ -49,4 +49,22 @@ QString Spreadsheet::formula(int row, int column) const {
     else
         return "";
 }
+
+void Spreadsheet::setFormula(int row, int column, const QString &formula) {
+    Cell *c = cell(row, column);
+    if (!c) {
+        c = new Cell;
+        setItem(row, column, c);
+    }
+    c->setFormula(formula);
+}
+
+QString Spreadsheet::currentLocation() const { return QChar('A' + currentColumn()) + QString::number(currentRow() + 1); }
+QString Spreadsheet::currentFormula() const { return formula(currentRow(), currentColumn()); }
+
+void Spreadsheet::somethingChanged() {
+    if (autoRecalc)
+        recalculate();
+    emit modified();
+}
 #pragma endregion //Subclassing QTableWidget
