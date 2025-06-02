537f0ec94c49395a820a0400bae8a9e0e9ee9ec7
apkretov <apkretov@hotmail.com>
2025-06-02 17:34:30 +0300

    spreadsheet.cpp

--- C++/Examples/C++ GUI Programming With Qt 4/04-01_Subclassing_QTableWidget/spreadsheet.cpp
+++ C++/Examples/C++ GUI Programming With Qt 4/04-01_Subclassing_QTableWidget/spreadsheet.cpp
@@ -1,1 +1,19 @@
 #include "spreadsheet.h"
+
+#pragma region Subclassing QTableWidget
+
+#include <QtGui>
+#include "cell.h"
+#include "spreadsheet.h"
+
+Spreadsheet::Spreadsheet(QWidget *parent) : QTableWidget(parent) {
+    autoRecalc = true;
+
+    setItemPrototype(new Cell);
+    setSelectionMode(ContiguousSelection);
+
+    connect(this, SIGNAL(itemChanged(QTableWidgetItem *)), this, SLOT(somethingChanged()));
+
+    clear();
+}
+#pragma endregion //Subclassing QTableWidget
