c3c60c2fa36f75361f9da310b4beda9e4f9dcf56
apkretov <apkretov@hotmail.com>
2025-06-02 19:11:23 +0300

    spreadsheet.cpp

--- C++/Examples/C++ GUI Programming With Qt 4/04-01_Subclassing_QTableWidget/spreadsheet.cpp
+++ C++/Examples/C++ GUI Programming With Qt 4/04-01_Subclassing_QTableWidget/spreadsheet.cpp
@@ -68,3 +68,60 @@ void Spreadsheet::somethingChanged() {
     emit modified();
 }
 #pragma endregion //Subclassing QTableWidget
+
+#pragma region Loading and Saving
+bool Spreadsheet::writeFile(const QString &fileName) {
+    QFile file(fileName);
+    if (!file.open(QIODevice::WriteOnly)) {
+        QMessageBox::warning(this, tr("Spreadsheet"), tr("Cannot write file %1:\n%2.").arg(file.fileName()).arg(file.errorString()));
+        return false;
+    }
+    QDataStream out(&file);
+    out.setVersion(QDataStream::Qt_4_3);
+
+    out << quint32(MagicNumber);
+
+    QApplication::setOverrideCursor(Qt::WaitCursor);
+    for (int row = 0; row < RowCount; ++row) {
+        for (int column = 0; column < ColumnCount; ++column) {
+            QString str = formula(row, column);
+            if (!str.isEmpty())
+                out << quint16(row) << quint16(column) << str;
+        }
+    }
+    QApplication::restoreOverrideCursor();
+    return true;
+}
+
+bool Spreadsheet::readFile(const QString &fileName) {
+    QFile file(fileName);
+    if (!file.open(QIODevice::ReadOnly)) {
+        QMessageBox::warning(this, tr("Spreadsheet"), tr("Cannot read file %1:\n%2.").arg(file.fileName()).arg(file.errorString()));
+        return false;
+    }
+
+    QDataStream in(&file);
+    in.setVersion(QDataStream::Qt_4_3);
+
+    quint32 magic;
+    in >> magic;
+    if (magic != MagicNumber) {
+        QMessageBox::warning(this, tr("Spreadsheet"), tr("The file is not a Spreadsheet file."));
+        return false;
+    }
+
+    clear();
+
+    quint16 row;
+    quint16 column;
+    QString str;
+
+    QApplication::setOverrideCursor(Qt::WaitCursor);
+    while (!in.atEnd()) {
+        in >> row >> column >> str;
+        setFormula(row, column, str);
+    }
+    QApplication::restoreOverrideCursor();
+    return true;
+}
+#pragma endregion //Loading and Saving
