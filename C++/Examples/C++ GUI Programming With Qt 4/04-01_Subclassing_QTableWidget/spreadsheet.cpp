ee4e13df8e0efab0d64954118e84cf631230758d
apkretov <apkretov@hotmail.com>
2025-06-02 17:58:55 +0300

    spreadsheet.cpp

--- C++/Examples/C++ GUI Programming With Qt 4/04-01_Subclassing_QTableWidget/spreadsheet.cpp
+++ C++/Examples/C++ GUI Programming With Qt 4/04-01_Subclassing_QTableWidget/spreadsheet.cpp
@@ -16,4 +16,37 @@ Spreadsheet::Spreadsheet(QWidget *parent) : QTableWidget(parent) {
 
     clear();
 }
+
+void Spreadsheet::clear() {
+    setRowCount(0);
+    setColumnCount(0);
+    setRowCount(RowCount);
+    setColumnCount(ColumnCount);
+
+    for (int i = 0; i < ColumnCount; ++i) {
+        QTableWidgetItem *item = new QTableWidgetItem;
+        item->setText(QString(QChar('A' + i)));
+        setHorizontalHeaderItem(i, item);
+    }
+
+    setCurrentCell(0, 0);
+}
+
+Cell *Spreadsheet::cell(int row, int column) const { return static_cast<Cell *>(item(row, column)); }
+
+QString Spreadsheet::text(int row, int column) const {
+    Cell *c = cell(row, column);
+    if (c)
+        return c->text();
+    else
+        return "";
+}
+
+QString Spreadsheet::formula(int row, int column) const {
+    Cell *c = cell(row, column);
+    if (c)
+        return c->formula();
+    else
+        return "";
+}
 #pragma endregion //Subclassing QTableWidget
