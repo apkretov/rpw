993491ef0009866cb35c34cbd16b9e6156aa9f33
apkretov <apkretov@hotmail.com>
2025-06-06 11:51:32 +0300

    editdialog.cpp

--- C++/Examples/Foundations Of Qt/02-02_The_header_file_for_the_ListDialog_class/editdialog.cpp
+++ C++/Examples/Foundations Of Qt/02-02_The_header_file_for_the_ListDialog_class/editdialog.cpp
@@ -1,5 +1,10 @@
 #include "editdialog.h"
-#include "ui_editdialog.h"
 
-EditDialog::EditDialog(QWidget *parent) : QDialog(parent), ui(new Ui::EditDialog) { ui->setupUi(this); }
-EditDialog::~EditDialog() { delete ui; }
+#pragma Listing 2-8. Editing an item of the list
+EditDialog::EditDialog(QWidget *parent) : QDialog(parent) { ui.setupUi(this); }
+#pragma endregion // Listing 2-8. Editing an item of the list
+
+#pragma region Listing 2-9. Editing an item of the list
+const QString EditDialog::name() const { return ui.nameEdit->text().replace("--", "").trimmed(); }
+void EditDialog::setName(const QString &name) { ui.nameEdit->setText(name); }
+#pragma endregion //Listing 2-9. Editing an item of the list
