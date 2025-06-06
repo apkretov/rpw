795a260e55ab14e3f44a5fa7698cf6524251548a
apkretov <apkretov@hotmail.com>
2025-06-06 11:53:04 +0300

    main.cpp

--- C++/Examples/Foundations Of Qt/02-02_The_header_file_for_the_ListDialog_class/main.cpp
+++ C++/Examples/Foundations Of Qt/02-02_The_header_file_for_the_ListDialog_class/main.cpp
@@ -1,11 +1,11 @@
+#include <QApplication>
 #include "listdialog.h"
 
-#include <QApplication>
-
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    ListDialog w;
-    w.show();
-    return a.exec();
+#pragma region Listing 2-10. Editing an item of the list
+int main(int argc, char **argv) {
+    QApplication app(argc, argv);
+    ListDialog dlg;
+    dlg.show();
+    return app.exec();
 }
+#pragma endregion // Listing 2-10. Editing an item of the list
