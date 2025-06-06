bd0e55863637f21d831a342b9839b51ed52eb0a8
apkretov <apkretov@hotmail.com>
2025-06-06 16:07:16 +0300

    Formatting

--- C++/Examples/Foundations Of Qt/02-02_The_header_file_for_the_ListDialog_class/editdialog.h
+++ C++/Examples/Foundations Of Qt/02-02_The_header_file_for_the_ListDialog_class/editdialog.h
@@ -5,13 +5,12 @@
 
 #pragma region Listing 2-7. Editing dialog class
 class EditDialog : public QDialog {
+    Ui::EditDialog ui;
 public:
     EditDialog(QWidget *parent = 0);
     const QString name() const;
     void setName(const QString &);
     const QString number() const;
     void setNumber(const QString &);
-private:
-    Ui::EditDialog ui;
 };
 #pragma endregion // Listing 2-7. Editing dialog class
