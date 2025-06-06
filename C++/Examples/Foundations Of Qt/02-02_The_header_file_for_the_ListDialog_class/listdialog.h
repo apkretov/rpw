f574d904d45283cea13dc03dec95c8774fe3a969
apkretov <apkretov@hotmail.com>
2025-06-06 14:20:20 +0300

    Formatting

--- C++/Examples/Foundations Of Qt/02-02_The_header_file_for_the_ListDialog_class/listdialog.h
+++ C++/Examples/Foundations Of Qt/02-02_The_header_file_for_the_ListDialog_class/listdialog.h
@@ -3,15 +3,15 @@
 #pragma region Listing 2-2. The header file for the ListDialog class
 #include <QDialog>
 #include "ui_listdialog.h"
+
 class ListDialog : public QDialog {
     Q_OBJECT
+    Ui::ListDialog ui;
 public:
     ListDialog();
 private slots:
     void addItem();
     void editItem();
     void deleteItem();
-private:
-    Ui::ListDialog ui;
 };
 #pragma endregion //Listing 2-2. The header file for the ListDialog class
