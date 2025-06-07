ba09053aa71edd1ce2c69592b46621f574814579
apkretov <apkretov@hotmail.com>
2025-06-07 19:22:15 +0300

    mydialog.h

--- C++/Examples/Foundations Of Qt/03-02_Setting_Size_Policies_in_Code/mydialog.h
+++ C++/Examples/Foundations Of Qt/03-02_Setting_Size_Policies_in_Code/mydialog.h
@@ -1,14 +1,27 @@
 #ifndef MYDIALOG_H
 #define MYDIALOG_H
 
-#include <QDialog>
+#include <QDialog>            
+#include <QGroupBox>         
+#include <QLabel>            
+#include <QLineEdit>         
+#include <QDialogButtonBox>      
+#include <QHBoxLayout>       
+#include <QVBoxLayout>       
+#include <QMessageBox>          
+#include <QtDebug>          
 
-class MyDialog : public QDialog
-{
+class MyDialog : public QDialog {
     Q_OBJECT
-
 public:
-    MyDialog(QWidget *parent = nullptr);
-    ~MyDialog();
+    explicit MyDialog(QWidget *parent = nullptr);
+private slots:
+    void handleAccepted();
+    void handleRejected();
+private:
+    QGroupBox *groupBox;
+    QLabel *label;
+    QLineEdit *lineEdit;
+    QDialogButtonBox *buttons;
 };
-#endif // MYDIALOG_H
+#endif  
