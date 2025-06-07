64c8a7692eecaf441c1f843f3c7e581bd74eb570
apkretov <apkretov@hotmail.com>
2025-06-07 19:26:45 +0300

    main.cpp

--- C++/Examples/Foundations Of Qt/03-02_Setting_Size_Policies_in_Code/main.cpp
+++ C++/Examples/Foundations Of Qt/03-02_Setting_Size_Policies_in_Code/main.cpp
@@ -1,11 +1,19 @@
-#include "mydialog.h"
+#include "MyDialog.h"   // Required for your custom dialog class
+#include <QApplication> // Required for QApplication [27]
 
-#include <QApplication>
+int main(int argc, char *argv[]) {
+    // Create a QApplication object. For all graphical Qt applications,
+    // there must be one (and only one) application instance [25, 28, 29].
+    QApplication app(argc, argv);
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MyDialog w;
-    w.show();
-    return a.exec();
+    // Create an instance of your custom dialog
+    MyDialog dialog;
+
+    // Show the dialog. For modal dialogs, exec() is commonly used,
+    // which starts the application's event loop and blocks until the dialog is
+    // closed [3, 23].
+    dialog.exec();
+
+    // When the dialog is closed, exec() returns, and the application exits [3].
+    return 0;
 }
