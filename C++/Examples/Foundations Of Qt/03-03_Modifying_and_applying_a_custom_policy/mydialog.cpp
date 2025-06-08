5937c8afb37da6e71040b9c4df9f0d517b48bb1b
apkretov <apkretov@hotmail.com>
2025-06-08 15:42:11 +0300

    03-03_Modifying_and_applying_a_custom_policy

---
+++ C++/Examples/Foundations Of Qt/03-03_Modifying_and_applying_a_custom_policy/mydialog.cpp
@@ -0,0 +1,48 @@
+#include "MyDialog.h"
+
+MyDialog::MyDialog(QWidget *parent) : QDialog(parent) {
+#pragma region Listing 3-1. The widgets are created.
+    groupBox = new QGroupBox(tr("Groupbox"), this);
+    label = new QLabel(tr("Supercalifragilisticexpialidocious"), this);
+    lineEdit = new QLineEdit(this);
+    buttons = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel, this);
+#pragma endregion //Listing 3-1. The widgets are created.
+
+#pragma region Listing 3-2. The widgets are laid out.
+    QHBoxLayout *hLayout = new QHBoxLayout(groupBox); // 2. Set up layouts (building upon Listing 3-2) // Horizontal layout for label and lineEdit inside the group box // 'groupBox' is parent for hLayout
+    hLayout->addWidget(label);
+    hLayout->addWidget(lineEdit);
+
+    QVBoxLayout *vLayout = new QVBoxLayout(this); // Vertical layout for the main dialog // 'this' sets MyDialog as parent for vLayout
+    vLayout->addWidget(groupBox);
+    vLayout->addStretch(); // Adds a stretchable space
+    vLayout->addWidget(buttons);
+#pragma endregion //Listing 3-2. The widgets are laid out.
+
+    connect(buttons, &QDialogButtonBox::accepted, this, &MyDialog::handleAccepted);
+    connect(buttons, &QDialogButtonBox::rejected, this, &MyDialog::handleRejected);
+
+    setLayout(vLayout); // Set the main layout for the dialog
+    setWindowTitle(tr("Qt Dialog Example")); // Set a title for the dialog window
+
+#pragma Listing 3-3. Modifying and applying a custom policy
+    QSizePolicy policy = label->sizePolicy();
+    policy.setHorizontalStretch( 3 );
+    label->setSizePolicy( policy );
+    policy = lineEdit->sizePolicy();
+    policy.setHorizontalStretch( 1 );
+    lineEdit->setSizePolicy( policy );
+#pragma endregion //Listing 3-3. Modifying and applying a custom policy
+}
+
+void MyDialog::handleAccepted() { // This slot is called when the OK button is clicked or accept() is implicitly called.
+    qDebug() << "OK button clicked!";
+    QMessageBox::information(this, tr("Dialog Status"), tr("You clicked OK!"));
+    accept(); // Closes the dialog with accepted status
+}
+
+void MyDialog::handleRejected() { // This slot is called when the Cancel button is clicked or reject() is implicitly called.
+    qDebug() << "Cancel button clicked!";
+    QMessageBox::warning(this, tr("Dialog Status"), tr("You clicked Cancel!"));
+    reject(); // Closes the dialog with rejected status
+}
---
+++ C++/Examples/Foundations Of Qt/03-03_Modifying_and_applying_a_custom_policy/mydialog.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <QDialog>
+#include <QGroupBox>
+#include <QLabel>
+#include <QLineEdit>
+#include <QDialogButtonBox>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QMessageBox>
+#include <QtDebug>
+
+class MyDialog : public QDialog {
+    Q_OBJECT
+public:
+    explicit MyDialog(QWidget *parent = nullptr);
+private slots:
+    void handleAccepted();
+    void handleRejected();
+private:
+    QGroupBox *groupBox;
+    QLabel *label;
+    QLineEdit *lineEdit;
+    QDialogButtonBox *buttons;
+};
+
