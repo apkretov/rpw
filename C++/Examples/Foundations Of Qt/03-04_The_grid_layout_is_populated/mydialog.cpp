303b604d71fad6a166ccd8a36f4e2c2f66ce4eb7
apkretov <apkretov@hotmail.com>
2025-06-08 15:57:41 +0300

    My attempt to implement Listing 3-4

--- C++/Examples/Foundations Of Qt/03-04_The_grid_layout_is_populated/mydialog.cpp
+++ C++/Examples/Foundations Of Qt/03-04_The_grid_layout_is_populated/mydialog.cpp
@@ -1,7 +1,46 @@
-#include "mydialog.h"
+#include "MyDialog.h"
 
-MyDialog::MyDialog(QWidget *parent)
-    : QDialog(parent)
-{}
+MyDialog::MyDialog(QWidget *parent) : QDialog(parent) {
+// #pragma region Listing 3-1. The widgets are created.
+//     groupBox = new QGroupBox(tr("Groupbox"), this);
+//     label = new QLabel(tr("Supercalifragilisticexpialidocious"), this);
+//     lineEdit = new QLineEdit(this);
+//     buttons = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel, this);
+// #pragma endregion //Listing 3-1. The widgets are created.
 
-MyDialog::~MyDialog() {}
+// #pragma region Listing 3-2. The widgets are laid out.
+//     QHBoxLayout *hLayout = new QHBoxLayout(groupBox); // 2. Set up layouts (building upon Listing 3-2) // Horizontal layout for label and lineEdit inside the group box // 'groupBox' is parent for hLayout
+//     hLayout->addWidget(label);
+//     hLayout->addWidget(lineEdit);
+
+//     QVBoxLayout *vLayout = new QVBoxLayout(this); // Vertical layout for the main dialog // 'this' sets MyDialog as parent for vLayout
+//     vLayout->addWidget(groupBox);
+//     vLayout->addStretch(); // Adds a stretchable space
+//     vLayout->addWidget(buttons);
+// #pragma endregion //Listing 3-2. The widgets are laid out.
+
+#pragma region Listing 3-4. The grid layout is populated.
+    QGridLayout layout(this); //ORIG QGridLayout layout(&widget);
+    layout.addWidget(new QPushButton("foo"), 0, 0, 1, 2);
+    layout.addWidget(new QPushButton("bar"), 1, 0);
+    layout.addWidget(new QPushButton("baz"), 1, 1);
+#pragma endregion //Listing 3-4. The grid layout is populated.
+
+    // connect(buttons, &QDialogButtonBox::accepted, this, &MyDialog::handleAccepted);
+    // connect(buttons, &QDialogButtonBox::rejected, this, &MyDialog::handleRejected);
+
+    // setLayout(vLayout);                      // Set the main layout for the dialog
+    // setWindowTitle(tr("Qt Dialog Example")); // Set a title for the dialog window
+}
+
+// void MyDialog::handleAccepted() { // This slot is called when the OK button is clicked or accept() is implicitly called.
+//     qDebug() << "OK button clicked!";
+//     QMessageBox::information(this, tr("Dialog Status"), tr("You clicked OK!"));
+//     accept(); // Closes the dialog with accepted status
+// }
+
+// void MyDialog::handleRejected() { // This slot is called when the Cancel button is clicked or reject() is implicitly called.
+//     qDebug() << "Cancel button clicked!";
+//     QMessageBox::warning(this, tr("Dialog Status"), tr("You clicked Cancel!"));
+//     reject(); // Closes the dialog with rejected status
+// }
--- C++/Examples/Foundations Of Qt/03-04_The_grid_layout_is_populated/mydialog.h
+++ C++/Examples/Foundations Of Qt/03-04_The_grid_layout_is_populated/mydialog.h
@@ -1,14 +1,28 @@
-#ifndef MYDIALOG_H
-#define MYDIALOG_H
+#pragma once
 
 #include <QDialog>
+// #include <QGroupBox>
+// #include <QLabel>
+// #include <QLineEdit>
+// #include <QDialogButtonBox>
+// #include <QHBoxLayout>
+// #include <QVBoxLayout>
+#include <QMessageBox>
+#include <QtDebug>
+#include <QPushButton>
+#include <QGridLayout>
 
-class MyDialog : public QDialog
-{
+class MyDialog : public QDialog {
     Q_OBJECT
-
 public:
-    MyDialog(QWidget *parent = nullptr);
-    ~MyDialog();
+    explicit MyDialog(QWidget *parent = nullptr);
+// private slots:
+//     void handleAccepted();
+//     void handleRejected();
+private:
+    // QGroupBox *groupBox;
+    // QLabel *label;
+    // QLineEdit *lineEdit;
+    // QDialogButtonBox *buttons;
 };
-#endif // MYDIALOG_H
+
