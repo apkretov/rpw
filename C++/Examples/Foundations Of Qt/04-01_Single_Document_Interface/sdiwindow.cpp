13160f655c0bfd79116fa622cee49cf55c70b00f
apkretov <apkretov@hotmail.com>
2025-06-09 13:57:51 +0300

    Commenting

--- C++/Examples/Foundations Of Qt/04-01_Single_Document_Interface/sdiwindow.cpp
+++ C++/Examples/Foundations Of Qt/04-01_Single_Document_Interface/sdiwindow.cpp
@@ -26,7 +26,7 @@ void SdiWindow::createActions() {
     newAction = new QAction(tr("&New"), this); //ORIG newAction = new QAction(QIcon(":/images/new.png"), tr("&New"), this);
     newAction->setShortcut(tr("Ctrl+N"));
     newAction->setStatusTip(tr("Create a new document"));
-    connect(newAction, SIGNAL(triggered()), this, SLOT(fileNew()));
+    connect(newAction, SIGNAL(triggered()), this, SLOT(fileNew())); //MINE connect(newAction, &QAction::triggered, this, &SdiWindow::fileNew);
     // ...
     cutAction = new QAction(tr("Cu&t"), this); //ORIG cutAction = new QAction(QIcon(":/images/cut.png"), tr("Cu&t"), this);
     cutAction->setShortcut(tr("Ctrl+X"));
