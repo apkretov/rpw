26188a866bc83d1abe963b2f895c3bbb585336be
apkretov <apkretov@hotmail.com>
2025-06-09 13:21:00 +0300

    Listing 4-2

--- C++/Examples/Foundations Of Qt/04-01_Single_Document_Interface/sdiwindow.cpp
+++ C++/Examples/Foundations Of Qt/04-01_Single_Document_Interface/sdiwindow.cpp
@@ -20,9 +20,27 @@ SdiWindow::SdiWindow(QWidget *parent) : QMainWindow(parent) {
 }
 #pragma endregion //Listing 4-1. Constructor of the SDI main window
 
-#pragma region MINE
-void SdiWindow::createActions() {}
+#pragma region Listing 4-2. Creating actions for the SDI application
+void SdiWindow::createActions() {
+    newAction = new QAction(QIcon(":/images/new.png"), tr("&New"), this);
+    newAction->setShortcut(tr("Ctrl+N"));
+    newAction->setStatusTip(tr("Create a new document"));
+    connect(newAction, SIGNAL(triggered()), this, SLOT(fileNew()));
+    // ...
+    cutAction = new QAction(QIcon(":/images/cut.png"), tr("Cu&t"), this);
+    cutAction->setShortcut(tr("Ctrl+X"));
+    cutAction->setStatusTip(tr("Cut"));
+    cutAction->setEnabled(false);
+    connect(docWidget, SIGNAL(copyAvailable(bool)), cutAction, SLOT(setEnabled(bool)));
+    connect(cutAction, SIGNAL(triggered()), docWidget, SLOT(cut()));
+    // ...
+    aboutQtAction = new QAction(tr("About &Qt"), this);
+    aboutQtAction->setStatusTip(tr("About the Qt toolkit"));
+    connect(aboutQtAction, SIGNAL(triggered()), qApp, SLOT(aboutQt()));
+}
+#pragma endregion //Listing 4-2. Creating actions for the SDI application
 
+#pragma region MINE
 void SdiWindow::createMenus() {}
 
 void SdiWindow::createToolbars() {}
