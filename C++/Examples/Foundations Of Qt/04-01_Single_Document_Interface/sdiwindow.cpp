e2916f76b813eb646045beba4b9bba39d9e1812d
apkretov <apkretov@hotmail.com>
2025-06-09 12:48:12 +0300

    Correcting errors

--- C++/Examples/Foundations Of Qt/04-01_Single_Document_Interface/sdiwindow.cpp
+++ C++/Examples/Foundations Of Qt/04-01_Single_Document_Interface/sdiwindow.cpp
@@ -1,10 +1,15 @@
 #include "sdiwindow.h"
 #include <QTextEdit>
+#include <QAction>
+#include <QMenu>
+#include <QMenuBar>
+#include <QToolBar>
+#include <QStatusBar>
 
 #pragma region Listing 4-1. Constructor of the SDI main window
 SdiWindow::SdiWindow(QWidget *parent) : QMainWindow(parent) {
     setAttribute(Qt::WA_DeleteOnClose);
-    setWindowTitle(QString("%1[*] - %2").arg("unnamed" -).arg(-"SDI"));
+    setWindowTitle(QString("%1[*] - %2").arg("unnamed").arg("SDI")); //ORIG setWindowTitle(QString("%1[*] - %2").arg("unnamed" -).arg(-"SDI"));
     docWidget = new QTextEdit(this);
     setCentralWidget(docWidget);
     connect(docWidget->document(), SIGNAL(modificationChanged(bool)), this, SLOT(setWindowModified(bool)));
@@ -14,3 +19,15 @@ SdiWindow::SdiWindow(QWidget *parent) : QMainWindow(parent) {
     statusBar()->showMessage("Done");
 }
 #pragma endregion //Listing 4-1. Constructor of the SDI main window
+
+void SdiWindow::createActions() {
+    // Implementation of createActions
+}
+
+void SdiWindow::createMenus() {
+    // Implementation of createMenus
+}
+
+void SdiWindow::createToolbars() {
+    // Implementation of createToolbars
+}
--- C++/Examples/Foundations Of Qt/04-01_Single_Document_Interface/sdiwindow.h
+++ C++/Examples/Foundations Of Qt/04-01_Single_Document_Interface/sdiwindow.h
@@ -2,8 +2,16 @@
 
 #include <QMainWindow>
 
+class QTextEdit;
+
 class SdiWindow : public QMainWindow {
     Q_OBJECT
 public:
     SdiWindow(QWidget *parent = nullptr);
+private:
+    QTextEdit *docWidget; //MINE
+    
+    void createActions();
+    void createMenus();
+    void createToolbars();
 };
