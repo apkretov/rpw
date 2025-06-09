7f05e88678e84f9ac4a865889b11635cf08673bd
apkretov <apkretov@hotmail.com>
2025-06-09 19:29:14 +0300

    Formatting

--- C++/Examples/Foundations Of Qt/04-02_Multiple_Document_Interface/mdiwindow.cpp
+++ C++/Examples/Foundations Of Qt/04-02_Multiple_Document_Interface/mdiwindow.cpp
@@ -7,8 +7,10 @@
 #pragma region Listing 4-8. Constructor of the main window with differences between MDI and SDI highlighted
 MdiWindow::MdiWindow(QWidget *parent) : QMainWindow(parent) {
     setWindowTitle(tr("MDI"));
+
     workspace = new QMdiArea; //ORIG workspace = new QWorkspace;
     setCentralWidget(workspace);
+
     connect(workspace, &QMdiArea::subWindowActivated, this, &MdiWindow::enableActions); //ORIG connect(workspace, SIGNAL(windowActivated(QWidget *)), this, SLOT(enableActions()));
     mapper = new QSignalMapper(this);
     connect(mapper, &QSignalMapper::mappedObject, this, [this](QObject *obj) {
@@ -16,10 +18,12 @@ MdiWindow::MdiWindow(QWidget *parent) : QMainWindow(parent) {
             if (QMdiSubWindow *window = qobject_cast<QMdiSubWindow*>(widget))
                 workspace->setActiveSubWindow(window);
     }); //ORIG connect(mapper, SIGNAL(mapped(QWidget *)), workspace, SLOT(setActiveWindow(QWidget *)));
+
     createActions();
     createMenus();
     createToolbars();
     statusBar()->showMessage( tr("Done") );
+
     enableActions();
 }
 #pragma endregion //Listing 4-8. Constructor of the main window with differences between MDI and SDI highlighted
