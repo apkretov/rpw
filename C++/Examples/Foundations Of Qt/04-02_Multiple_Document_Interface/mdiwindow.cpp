981615a04cab4570072c685453495e4d328869f6
apkretov <apkretov@hotmail.com>
2025-06-09 19:28:19 +0300

    Correcting errors

--- C++/Examples/Foundations Of Qt/04-02_Multiple_Document_Interface/mdiwindow.cpp
+++ C++/Examples/Foundations Of Qt/04-02_Multiple_Document_Interface/mdiwindow.cpp
@@ -1,14 +1,21 @@
 #include "mdiwindow.h"
-#include <QWorkspace>
+#include <QMdiArea>
+#include <QMdiSubWindow>
+#include <QSignalMapper>
+#include <QStatusBar>
 
 #pragma region Listing 4-8. Constructor of the main window with differences between MDI and SDI highlighted
 MdiWindow::MdiWindow(QWidget *parent) : QMainWindow(parent) {
     setWindowTitle(tr("MDI"));
-    workspace = new QWorkspace;
+    workspace = new QMdiArea; //ORIG workspace = new QWorkspace;
     setCentralWidget(workspace);
-    connect(workspace, SIGNAL(windowActivated(QWidget *)), this, SLOT(enableActions()));
+    connect(workspace, &QMdiArea::subWindowActivated, this, &MdiWindow::enableActions); //ORIG connect(workspace, SIGNAL(windowActivated(QWidget *)), this, SLOT(enableActions()));
     mapper = new QSignalMapper(this);
-    connect(mapper, SIGNAL(mapped(QWidget *)), workspace, SLOT(setActiveWindow(QWidget *)));
+    connect(mapper, &QSignalMapper::mappedObject, this, [this](QObject *obj) {
+        if (QWidget *widget = qobject_cast<QWidget*>(obj))
+            if (QMdiSubWindow *window = qobject_cast<QMdiSubWindow*>(widget))
+                workspace->setActiveSubWindow(window);
+    }); //ORIG connect(mapper, SIGNAL(mapped(QWidget *)), workspace, SLOT(setActiveWindow(QWidget *)));
     createActions();
     createMenus();
     createToolbars();
--- C++/Examples/Foundations Of Qt/04-02_Multiple_Document_Interface/mdiwindow.h
+++ C++/Examples/Foundations Of Qt/04-02_Multiple_Document_Interface/mdiwindow.h
@@ -2,8 +2,19 @@
 
 #include <QMainWindow>
 
+class QMdiArea;
+class QSignalMapper;
+
 class MdiWindow : public QMainWindow {
     Q_OBJECT
 public:
     MdiWindow(QWidget *parent = nullptr);
+private:
+    QMdiArea *workspace;
+    QSignalMapper *mapper;
+private slots:
+    void enableActions() {}
+    void createActions() {}
+    void createMenus() {}
+    void createToolbars() {}
 };
