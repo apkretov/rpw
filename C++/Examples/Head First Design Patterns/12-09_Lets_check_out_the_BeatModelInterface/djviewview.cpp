612da2ee501ab0a925af47d98cb04ff8d9c8e9c0
apkretov <apkretov@hotmail.com>
2025-06-15 22:45:34 +0300

    Removing djviewview.cpp

--- C++/Examples/Head First Design Patterns/12-09_Lets_check_out_the_BeatModelInterface/djviewview.cpp
+++
@@ -1,6 +0,0 @@
-// #include "DJViewView.h"
-// #include "ui_djviewview.h"
-
-// DJViewView::DJViewView(QWidget *parent) : QWidget(parent) , ui(new Ui::DJViewView) { ui->setupUi(this); }
-
-// DJViewView::~DJViewView() { delete ui; }
