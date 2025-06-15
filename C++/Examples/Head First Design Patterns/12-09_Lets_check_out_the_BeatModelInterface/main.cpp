e6df484a1d40c4e7ec342e04882c148cfd53f322
apkretov <apkretov@hotmail.com>
2025-06-15 19:30:39 +0300

    Refactoring

--- C++/Examples/Head First Design Patterns/12-09_Lets_check_out_the_BeatModelInterface/main.cpp
+++ C++/Examples/Head First Design Patterns/12-09_Lets_check_out_the_BeatModelInterface/main.cpp
@@ -1,11 +1,23 @@
+#ifdef OFF
 #include "DJViewControl.h"
+#endif //OFF
 
 #include <QApplication>
 
-int main(int argc, char *argv[])
-{
+void Example_Observer_Pattern_Less_Coupled_Extended_Demo();
+
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+
+#ifdef OFF
     DJView w;
     w.show();
     return a.exec();
+#endif //OFF
+    Example_Observer_Pattern_Less_Coupled_Extended_Demo();
+}
+
+#include "Example_Observer_Pattern_Less_Coupled_Extended.h"
+void Example_Observer_Pattern_Less_Coupled_Extended_Demo() {
+    Demo();
 }
