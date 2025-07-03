8c667e92f1aa797ef29f6157b41d4b096210dff6
apkretov <apkretov@hotmail.com>
2025-07-03 17:31:54 +0300

    Correcting errors

--- C++/Examples/Head First Design Patterns/12-09_Lets_check_out_the_BeatModelInterface/main.cpp
+++ C++/Examples/Head First Design Patterns/12-09_Lets_check_out_the_BeatModelInterface/main.cpp
@@ -8,6 +8,7 @@ int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
 
     BeatModel beatModel;
+    beatModel.initialize();
     BeatController beatController{beatModel};
 
     DJViewView view{beatModel};
