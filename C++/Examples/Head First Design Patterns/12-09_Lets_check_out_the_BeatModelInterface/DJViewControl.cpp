3e30714344eedb4fd8d654ed50aabaf6ffbb660d
apkretov <apkretov@hotmail.com>
2025-07-04 12:50:33 +0300

    Refactoring

--- C++/Examples/Head First Design Patterns/12-09_Lets_check_out_the_BeatModelInterface/DJViewControl.cpp
+++ C++/Examples/Head First Design Patterns/12-09_Lets_check_out_the_BeatModelInterface/DJViewControl.cpp
@@ -1,16 +1,25 @@
 #include "DJViewControl.h"
 #include "ControllerInterface.h"
 
-DJViewControl::DJViewControl(ControllerInterface& controller, QWidget *parent) : QMainWindow(parent), ui(new Ui::DJView), controller(controller) { ui->setupUi(this); }
+DJViewControl::DJViewControl(ControllerInterface& controller, QWidget *parent) : QMainWindow(parent), ui(new Ui::DJView), controller(controller) {
+    ui->setupUi(this);
+    
+    // Explicit connections
+    connect(ui->setBPMButton, &QPushButton::clicked, this, &DJViewControl::setBPM);
+    connect(ui->increaseBPMButton, &QPushButton::clicked, this, &DJViewControl::increaseBPM);
+    connect(ui->decreaseBPMButton, &QPushButton::clicked, this, &DJViewControl::decreaseBPM);
+    connect(ui->actionStart, &QAction::triggered, this, &DJViewControl::start);
+    connect(ui->actionStop, &QAction::triggered, this, &DJViewControl::stop);
+}
 
 DJViewControl::~DJViewControl() { delete ui; }
 
-void DJViewControl::on_setBPMButton_clicked() { controller.setBPM(ui->bpmTextField->text().toInt()); }
+void DJViewControl::setBPM() { controller.setBPM(ui->bpmTextField->text().toInt()); }
 
-void DJViewControl::on_increaseBPMButton_clicked() { controller.increaseBPM(); }
+void DJViewControl::increaseBPM() { controller.increaseBPM(); }
 
-void DJViewControl::on_decreaseBPMButton_clicked() { controller.decreaseBPM(); }
+void DJViewControl::decreaseBPM() { controller.decreaseBPM(); }
 
-void DJViewControl::on_actionStart_triggered() { controller.start(); }
+void DJViewControl::start() { controller.start(); }
 
-void DJViewControl::on_actionStop_triggered() { controller.stop(); }
+void DJViewControl::stop() { controller.stop(); }
--- C++/Examples/Head First Design Patterns/12-09_Lets_check_out_the_BeatModelInterface/DJViewControl.h
+++ C++/Examples/Head First Design Patterns/12-09_Lets_check_out_the_BeatModelInterface/DJViewControl.h
@@ -17,11 +17,11 @@ public:
     DJViewControl(ControllerInterface& controller, QWidget *parent = nullptr);
     ~DJViewControl();
 private slots:
-    void on_setBPMButton_clicked();
-    void on_increaseBPMButton_clicked();
-    void on_decreaseBPMButton_clicked();
-    void on_actionStart_triggered();
-    void on_actionStop_triggered();
+    void setBPM();
+    void increaseBPM();
+    void decreaseBPM();
+    void start();
+    void stop();
 private:
     Ui::DJView *ui;
     ControllerInterface& controller;
