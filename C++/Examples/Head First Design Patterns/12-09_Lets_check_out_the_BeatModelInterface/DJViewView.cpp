3afb32a35da8107927798f8214919b1d038fb4a7
apkretov <apkretov@hotmail.com>
2025-07-04 12:32:38 +0300

    Formatting

--- C++/Examples/Head First Design Patterns/12-09_Lets_check_out_the_BeatModelInterface/DJViewView.cpp
+++ C++/Examples/Head First Design Patterns/12-09_Lets_check_out_the_BeatModelInterface/DJViewView.cpp
@@ -1,26 +1,13 @@
 #include "DJViewView.h"
 
-DJViewView::DJViewView(BeatModelInterface& model, QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::DJViewView),
-    model(model)
-{
+DJViewView::DJViewView(BeatModelInterface& model, QWidget *parent) : QWidget(parent), ui(new Ui::DJViewView), model(model) {
     ui->setupUi(this);
     model.registerObserver((BPMObserver*)this);
     model.registerObserver((BeatObserver*)this);
 }
 
-DJViewView::~DJViewView()
-{
-    delete ui;
-}
+DJViewView::~DJViewView() { delete ui; }
 
-void DJViewView::updateBPM()
-{
-    ui->bpmOutputLabel->setText(QString::number(model.getBPM()));
-}
+void DJViewView::updateBPM() { ui->bpmOutputLabel->setText(QString::number(model.getBPM())); }
 
-void DJViewView::updateBeat()
-{
-    ui->beatBar->setValue(100);
-}
+void DJViewView::updateBeat() { ui->beatBar->setValue(100); }
