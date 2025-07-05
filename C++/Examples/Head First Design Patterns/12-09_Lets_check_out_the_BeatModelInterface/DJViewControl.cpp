b390b39f4b31320ea98fdb6ea4d8822c197d7af6
apkretov <apkretov@hotmail.com>
2025-07-05 16:03:55 +0300

    Formatting

--- C++/Examples/Head First Design Patterns/12-09_Lets_check_out_the_BeatModelInterface/DJViewControl.cpp
+++ C++/Examples/Head First Design Patterns/12-09_Lets_check_out_the_BeatModelInterface/DJViewControl.cpp
@@ -6,42 +6,19 @@ DJViewControl::DJViewControl(ControllerInterface& controller, QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::DJView),
     controller(controller)
-{
-    ui->setupUi(this);
-}
+{ ui->setupUi(this); }
 
-DJViewControl::~DJViewControl()
-{
-    delete ui;
-}
+DJViewControl::~DJViewControl() { delete ui; }
 
-void DJViewControl::on_setBPMButton_clicked()
-{
-    controller.setBPM(ui->bpmTextField->text().toInt());
-}
+void DJViewControl::on_setBPMButton_clicked() { controller.setBPM(ui->bpmTextField->text().toInt()); }
 
-void DJViewControl::on_increaseBPMButton_clicked()
-{
-    controller.increaseBPM();
-}
+void DJViewControl::on_increaseBPMButton_clicked() { controller.increaseBPM(); }
 
-void DJViewControl::on_decreaseBPMButton_clicked()
-{
-    controller.decreaseBPM();
-}
+void DJViewControl::on_decreaseBPMButton_clicked() { controller.decreaseBPM(); }
 
-void DJViewControl::on_actionStart_triggered()
-{
-    controller.start();
-}
+void DJViewControl::on_actionStart_triggered() { controller.start(); }
 
-void DJViewControl::on_actionStop_triggered()
-{
-    controller.stop();
-}
+void DJViewControl::on_actionStop_triggered() { controller.stop(); }
 
-void DJViewControl::on_actionQuit_triggered()
-{
-    qApp->quit();
-}
+void DJViewControl::on_actionQuit_triggered() { qApp->quit(); }
 
