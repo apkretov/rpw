a16acbe9aba486f977a067938d3530fcbec99ac6
apkretov <apkretov@hotmail.com>
2025-07-06 13:56:26 +0300

    Moving DJViewControl.cpp into DJViewControl.h

--- C++/Examples/Head First Design Patterns/12-09_Lets_check_out_the_BeatModelInterface/DJViewControl.h
+++ C++/Examples/Head First Design Patterns/12-09_Lets_check_out_the_BeatModelInterface/DJViewControl.h
@@ -2,6 +2,8 @@
 
 #include <QMainWindow>
 #include "./ui_djviewcontrol.h"
+#include "ControllerInterface.h"
+#include <QApplication>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -9,20 +11,33 @@ namespace Ui {
 }
 QT_END_NAMESPACE
 
-class ControllerInterface;
-
 class DJViewControl : public QMainWindow {
     Q_OBJECT
 public:
-    DJViewControl(ControllerInterface& controller, QWidget *parent = nullptr);
-    ~DJViewControl();
+    DJViewControl(ControllerInterface& controller, QWidget *parent = nullptr) :
+        QMainWindow(parent),
+        ui(new Ui::DJView),
+        controller(controller)
+    {
+        ui->setupUi(this);
+        connect(ui->setBPMButton, &QPushButton::clicked, this, &DJViewControl::setBPM);
+        connect(ui->increaseBPMButton, &QPushButton::clicked, this, &DJViewControl::increaseBPM);
+        connect(ui->decreaseBPMButton, &QPushButton::clicked, this, &DJViewControl::decreaseBPM);
+        connect(ui->actionStart, &QAction::triggered, this, &DJViewControl::start);
+        connect(ui->actionStop, &QAction::triggered, this, &DJViewControl::stop);
+        connect(ui->actionQuit, &QAction::triggered, this, &DJViewControl::quit);
+    }
+
+    ~DJViewControl() { delete ui; }
+
 private slots:
-    void setBPM();
-    void increaseBPM();
-    void decreaseBPM();
-    void start();
-    void stop();
-    void quit();
+    void setBPM() { controller.setBPM(ui->bpmTextField->text().toInt()); }
+    void increaseBPM() { controller.increaseBPM(); }
+    void decreaseBPM() { controller.decreaseBPM(); }
+    void start() { controller.start(); }
+    void stop() { controller.stop(); }
+    void quit() { qApp->quit(); }
+
 private:
     Ui::DJView *ui;
     ControllerInterface& controller;
