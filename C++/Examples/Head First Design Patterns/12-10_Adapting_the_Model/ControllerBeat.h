121bfbbd82939dad965c89659b841cf305d8b05e
apkretov <apkretov@hotmail.com>
2025-07-07 15:27:37 +0300

    Removing QTimer

--- C++/Examples/Head First Design Patterns/12-10_Adapting_the_Model/ControllerBeat.h
+++ C++/Examples/Head First Design Patterns/12-10_Adapting_the_Model/ControllerBeat.h
@@ -1,26 +1,28 @@
 #pragma once
 
-#include <QObject>
-#include <QTimer>
+//#include <QObject>
+//#include <QTimer>
 #include "ControllerInterface.h"
 #include "BeatModelInterface.h"
 
-class ControllerBeat : public QObject, public ControllerInterface {
-    Q_OBJECT
+//class ControllerBeat : public QObject, public ControllerInterface {
+class ControllerBeat : public ControllerInterface {
+    //Q_OBJECT
     BeatModelInterface& model;
-    QTimer* timer;
+    //QTimer* timer;
 public:
-    ControllerBeat(BeatModelInterface& model, QObject *parent = nullptr) : QObject(parent), model(model), timer(new QTimer(this))
-    { connect(timer, &QTimer::timeout, this, &ControllerBeat::beat); }
+    //ControllerBeat(BeatModelInterface& model, QObject *parent = nullptr) : QObject(parent), model(model), timer(new QTimer(this))
+    //{ connect(timer, &QTimer::timeout, this, &ControllerBeat::beat); }
+	ControllerBeat(BeatModelInterface &model) : model(model) {}
 
     void start() override {
         model.on();
-        timer->start(60000 / model.getBPM());
+        //timer->start(60000 / model.getBPM());
     }
 
     void stop() override {
         model.off();
-        timer->stop();
+        //timer->stop();
     }
 
     void increaseBPM() override {
@@ -35,11 +37,11 @@ public:
 
     void setBPM(int bpm) override {
         model.setBPM(bpm);
-        if (bpm > 0)
-            timer->setInterval(60000 / bpm);
-        else
-            timer->stop();
+        //if (bpm > 0)
+        //    timer->setInterval(60000 / bpm);
+        //else
+        //    timer->stop();
     }
-private slots:
-    void beat() { model.beatEvent(); }
+//private slots:
+//    void beat() { model.beatEvent(); }
 };
