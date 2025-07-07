56c526b9efd0465f3c8db1b68137970b8d0a89c6
apkretov <apkretov@hotmail.com>
2025-07-07 15:22:47 +0300

    Formatting

--- C++/Examples/Head First Design Patterns/12-09_Lets_check_out_the_BeatModelInterface/ControllerBeat.h
+++ C++/Examples/Head First Design Patterns/12-09_Lets_check_out_the_BeatModelInterface/ControllerBeat.h
@@ -1,28 +1,14 @@
 #pragma once
 
-// #include <QObject>
-// #include <QTimer>
 #include "ControllerInterface.h"
 #include "BeatModelInterface.h"
 
-class ControllerBeat : public QObject, public ControllerInterface {
-    //Q_OBJECT
+class ControllerBeat : public ControllerInterface {
     BeatModelInterface& model;
-    // QTimer* timer;
 public:
-    // ControllerBeat(BeatModelInterface& model, QObject *parent = nullptr) : QObject(parent), model(model), timer(new QTimer(this))
-    // { connect(timer, &QTimer::timeout, this, &ControllerBeat::beat); }
     ControllerBeat(BeatModelInterface& model) : model(model) {}
-
-    void start() override {
-        model.on();
-        // timer->start(60000 / model.getBPM());
-    }
-
-    void stop() override {
-        model.off();
-        // timer->stop();
-    }
+    void start() override { model.on(); }
+    void stop() override { model.off(); }
 
     void increaseBPM() override {
         int bpm = model.getBPM();
@@ -34,13 +20,5 @@ public:
         model.setBPM(bpm - 1);
     }
 
-    void setBPM(int bpm) override {
-        model.setBPM(bpm);
-        // if (bpm > 0)
-        //     timer->setInterval(60000 / bpm);
-        // else
-        //     timer->stop();
-    }
-// private slots:
-    void beat() { model.beatEvent(); }
+    void setBPM(int bpm) override { model.setBPM(bpm); }
 };
