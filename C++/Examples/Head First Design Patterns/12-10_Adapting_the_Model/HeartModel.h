7ae67752ce1a4ac312bcd670b2ee5fe0727855aa
apkretov <apkretov@hotmail.com>
2025-07-07 16:40:09 +0300

    Refactoring

--- C++/Examples/Head First Design Patterns/12-10_Adapting_the_Model/HeartModel.h
+++ C++/Examples/Head First Design Patterns/12-10_Adapting_the_Model/HeartModel.h
@@ -10,9 +10,8 @@ class HeartModel : public QObject, public HeartModelInterface {
     Q_OBJECT
     std::vector<BeatObserver*> beatObservers;
     std::vector<BPMObserver*> bpmObservers;
-    int heartRate = 90;
+    int heartRate = 68;
     QTimer* timer;
-
 public:
     HeartModel(QObject* parent = nullptr) : QObject(parent) {
         timer = new QTimer(this);
@@ -20,25 +19,11 @@ public:
         timer->start(1000);
     }
 
-    int getHeartRate() const override {
-        return heartRate;
-    }
-
-    void registerObserver(BeatObserver* o) override {
-        beatObservers.push_back(o);
-    }
-
-    void removeObserver(BeatObserver* o) override {
-        beatObservers.erase(std::remove(beatObservers.begin(), beatObservers.end(), o), beatObservers.end());
-    }
-
-    void registerObserver(BPMObserver* o) override {
-        bpmObservers.push_back(o);
-    }
-
-    void removeObserver(BPMObserver* o) override {
-        bpmObservers.erase(std::remove(bpmObservers.begin(), bpmObservers.end(), o), bpmObservers.end());
-    }
+    int getHeartRate() const override { return heartRate; }
+    void registerObserver(BeatObserver* o) override { beatObservers.push_back(o); }
+    void removeObserver(BeatObserver* o) override { beatObservers.erase(std::remove(beatObservers.begin(), beatObservers.end(), o), beatObservers.end()); }
+    void registerObserver(BPMObserver* o) override { bpmObservers.push_back(o); }
+    void removeObserver(BPMObserver* o) override { bpmObservers.erase(std::remove(bpmObservers.begin(), bpmObservers.end(), o), bpmObservers.end()); }
 
     void notifyBeatObservers() {
         for (BeatObserver* observer : beatObservers) {
@@ -53,7 +38,8 @@ public:
     }
 public slots:
     void beat() {
-        heartRate = rand() % 10 + 90;
+		const int variation = 10;
+        heartRate = heartRate - (variation / 2) + rand() % variation;
         notifyBPMObservers();
         notifyBeatObservers();
     }
