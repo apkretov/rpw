120a7143bbef0ffe1a77f1747b6f3fd1f6b22ff4
apkretov <apkretov@hotmail.com>
2025-07-07 14:10:15 +0300

    Removing DJView.h

--- C++/Examples/Head First Design Patterns/12-10_Adapting_the_Model/DJViewControl.h
+++ C++/Examples/Head First Design Patterns/12-10_Adapting_the_Model/DJViewControl.h
@@ -5,6 +5,86 @@
 #include "ControllerInterface.h"
 #include <QApplication>
 
+#pragma region Implementing the View
+/* Java @ https://github.com/bethrobson/Head-First-Design-Patterns/tree/master/src/headfirst/designpatterns/combined/djview
+public class DJView implements ActionListener,  BeatObserver, BPMObserver {
+	BeatModelInterface model;
+	ControllerInterface controller;
+	JFrame viewFrame;
+	JPanel viewPanel;
+	BeatBar beatBar;
+	JLabel bpmOutputLabel;
+	JFrame controlFrame;
+	JPanel controlPanel;
+	JLabel bpmLabel;
+	JTextField bpmTextField;
+	JButton setBPMButton;
+	JButton increaseBPMButton;
+	JButton decreaseBPMButton;
+	JMenuBar menuBar;
+	JMenu menu;
+	JMenuItem startMenuItem;
+	JMenuItem stopMenuItem;
+
+	public DJView(ControllerInterface controller, BeatModelInterface model) {
+		this.controller = controller;
+		this.model = model;
+		model.registerObserver((BeatObserver)this);
+		model.registerObserver((BPMObserver)this);
+	}
+
+	public void createView() {
+		// Create all Swing components here
+	}
+
+	public void createControls() {
+		// Create all Swing components here
+	}
+
+	public void enableStopMenuItem() {
+		stopMenuItem.setEnabled(true);
+	}
+
+	public void disableStopMenuItem() {
+		stopMenuItem.setEnabled(false);
+	}
+
+	public void enableStartMenuItem() {
+		startMenuItem.setEnabled(true);
+	}
+
+	public void disableStartMenuItem() {
+		startMenuItem.setEnabled(false);
+	}
+
+	public void actionPerformed(ActionEvent event) {
+		if (event.getSource() == setBPMButton) {
+			int bpm = Integer.parseInt(bpmTextField.getText());
+			controller.setBPM(bpm);
+		} else if (event.getSource() == increaseBPMButton) {
+			controller.increaseBPM();
+		} else if (event.getSource() == decreaseBPMButton) {
+			controller.decreaseBPM();
+		}
+	}
+
+	public void updateBPM() {
+		if (model != null) {
+			int bpm = model.getBPM();
+			if (bpm == 0) {
+				bpmOutputLabel.setText("offline");
+			} else {
+				bpmOutputLabel.setText("Current BPM: " + model.getBPM());
+			}
+		}
+	}
+
+	public void updateBeat() {
+		beatBar.setValue(100);
+	}
+}
+*/
+
 QT_BEGIN_NAMESPACE
 namespace Ui {
     class DJView;
@@ -51,3 +131,4 @@ private slots:
 
     void quit() { qApp->quit(); }
 };
+#pragma endregion //Implementing the View
--- C++/Examples/Head First Design Patterns/12-10_Adapting_the_Model/DJViewView.h
+++ C++/Examples/Head First Design Patterns/12-10_Adapting_the_Model/DJViewView.h
@@ -7,6 +7,86 @@
 #include "ObserverBeat.h"
 #include "BeatModelInterface.h"
 
+#pragma region Implementing the View
+/* Java @ https://github.com/bethrobson/Head-First-Design-Patterns/tree/master/src/headfirst/designpatterns/combined/djview
+public class DJView implements ActionListener,  BeatObserver, BPMObserver {
+	BeatModelInterface model;
+	ControllerInterface controller;
+	JFrame viewFrame;
+	JPanel viewPanel;
+	BeatBar beatBar;
+	JLabel bpmOutputLabel;
+	JFrame controlFrame;
+	JPanel controlPanel;
+	JLabel bpmLabel;
+	JTextField bpmTextField;
+	JButton setBPMButton;
+	JButton increaseBPMButton;
+	JButton decreaseBPMButton;
+	JMenuBar menuBar;
+	JMenu menu;
+	JMenuItem startMenuItem;
+	JMenuItem stopMenuItem;
+
+	public DJView(ControllerInterface controller, BeatModelInterface model) {
+		this.controller = controller;
+		this.model = model;
+		model.registerObserver((BeatObserver)this);
+		model.registerObserver((BPMObserver)this);
+	}
+
+	public void createView() {
+		// Create all Swing components here
+	}
+
+	public void createControls() {
+		// Create all Swing components here
+	}
+
+	public void enableStopMenuItem() {
+		stopMenuItem.setEnabled(true);
+	}
+
+	public void disableStopMenuItem() {
+		stopMenuItem.setEnabled(false);
+	}
+
+	public void enableStartMenuItem() {
+		startMenuItem.setEnabled(true);
+	}
+
+	public void disableStartMenuItem() {
+		startMenuItem.setEnabled(false);
+	}
+
+	public void actionPerformed(ActionEvent event) {
+		if (event.getSource() == setBPMButton) {
+			int bpm = Integer.parseInt(bpmTextField.getText());
+			controller.setBPM(bpm);
+		} else if (event.getSource() == increaseBPMButton) {
+			controller.increaseBPM();
+		} else if (event.getSource() == decreaseBPMButton) {
+			controller.decreaseBPM();
+		}
+	}
+
+	public void updateBPM() {
+		if (model != null) {
+			int bpm = model.getBPM();
+			if (bpm == 0) {
+				bpmOutputLabel.setText("offline");
+			} else {
+				bpmOutputLabel.setText("Current BPM: " + model.getBPM());
+			}
+		}
+	}
+
+	public void updateBeat() {
+		beatBar.setValue(100);
+	}
+}
+*/
+
 namespace Ui {
     class DJViewView;
 }
@@ -40,3 +120,4 @@ public slots:
         QTimer::singleShot(50, this, [this]() { ui->beatBar->setValue(ui->beatBar->minimum()); });
     }
 };
+#pragma endregion //Implementing the View
