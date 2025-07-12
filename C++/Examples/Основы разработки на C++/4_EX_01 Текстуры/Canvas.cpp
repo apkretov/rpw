e78b8bd3d77003ae64a68ac9614e0d06697146eb
apkretov <apkretov@hotmail.com>
2025-07-12 13:40:14 +0300

    Canvas.cpp

---
+++ C++/Examples/Основы разработки на C++/4_EX_01 Текстуры/Canvas.cpp
@@ -0,0 +1,60 @@
+#include "Canvas.h"
+
+void Canvas::SetSize(Size size) { 
+	size_ = size; 
+}
+
+Canvas::ShapeId Canvas::AddShape(ShapeType shape_type, Point position, Size size, unique_ptr<ITexture> texture) {
+	auto shape = MakeShape(shape_type);
+	shape->SetPosition(position);
+	shape->SetSize(size);
+	shape->SetTexture(std::move(texture));
+	return InsertShape(std::move(shape));
+}
+
+Canvas::ShapeId Canvas::DuplicateShape(ShapeId source_id, Point target_position) {
+	auto shape = GetShapeNodeById(source_id)->second->Clone();
+	shape->SetPosition(target_position);
+	return InsertShape(std::move(shape));
+}
+
+void Canvas::RemoveShape(ShapeId id) { 
+	shapes_.erase(GetShapeNodeById(id)); 
+}
+
+void Canvas::MoveShape(ShapeId id, Point position) { 
+	GetShapeNodeById(id)->second->SetPosition(position); 
+}
+
+void Canvas::ResizeShape(ShapeId id, Size size) { 
+	GetShapeNodeById(id)->second->SetSize(size); 
+}
+
+int Canvas::GetShapesCount() const { 
+	return static_cast<int>(shapes_.size()); 
+}
+
+void Canvas::Print(ostream &output) const {
+	Image image(size_.height, string(size_.width, ' '));
+
+	for (const auto &[id, shape] : shapes_)
+		shape->Draw(image);
+
+	output << '#' << string(size_.width, '#') << "#\n";
+	for (const auto &line : image)
+		output << '#' << line << "#\n";
+	output << '#' << string(size_.width, '#') << "#\n";
+}
+
+Canvas::Shapes::iterator Canvas::GetShapeNodeById(ShapeId id) {
+	auto it = shapes_.find(id);
+	if (it == shapes_.end()) {
+		throw std::out_of_range("No shape with given ID");
+	}
+	return it;
+}
+
+Canvas::ShapeId Canvas::InsertShape(unique_ptr<IShape> shape) {
+	shapes_[current_id_] = std::move(shape);
+	return current_id_++;
+}
--- C++/Examples/Основы разработки на C++/4_EX_01 Текстуры/Canvas.h
+++ C++/Examples/Основы разработки на C++/4_EX_01 Текстуры/Canvas.h
@@ -3,63 +3,26 @@
 #include <map>
 #include <iostream>
 #include "common.h"
-#include "Canvas.h"
-#include "Ellipse.h"
 #include "IShape.h"
 #include "ITexture.h"
-#include "Rectangle.h"
 using namespace std;
 
-class Canvas { // Автоматизированный редактор графики, который управляется через открытые методы.
+class Canvas {
 public:
 	using ShapeId = size_t;
-	void SetSize(Size size) { size_ = size; }
+	void SetSize(Size size);
+	ShapeId AddShape(ShapeType shape_type, Point position, Size size, unique_ptr<ITexture> texture);
+	ShapeId DuplicateShape(ShapeId source_id, Point target_position);
+	void RemoveShape(ShapeId id);
+	void MoveShape(ShapeId id, Point position);
+	void ResizeShape(ShapeId id, Size size);
+	int GetShapesCount() const;
+	void Print(ostream &output) const;
 
-	ShapeId AddShape(ShapeType shape_type, Point position, Size size, unique_ptr<ITexture> texture) { // Добавляет новую фигуру с помощью функции MakeShape().
-		auto shape = MakeShape(shape_type);
-		shape->SetPosition(position);
-		shape->SetSize(size);
-		shape->SetTexture(std::move(texture));
-		return InsertShape(std::move(shape));
-	}
-
-	ShapeId DuplicateShape(ShapeId source_id, Point target_position) { // Клонирует существующую фигуру, вызывая метод Clone().
-		auto shape = GetShapeNodeById(source_id)->second->Clone();
-		shape->SetPosition(target_position);
-		return InsertShape(std::move(shape));
-	}
-
-	void RemoveShape(ShapeId id) { shapes_.erase(GetShapeNodeById(id)); }
-	void MoveShape(ShapeId id, Point position) { GetShapeNodeById(id)->second->SetPosition(position); }
-	void ResizeShape(ShapeId id, Size size) { GetShapeNodeById(id)->second->SetSize(size); }
-	int GetShapesCount() const { return static_cast<int>(shapes_.size()); }
-
-	void Print(ostream &output) const {
-		Image image(size_.height, string(size_.width, ' '));
-
-		for (const auto &[id, shape] : shapes_)
-			shape->Draw(image);
-
-		output << '#' << string(size_.width, '#') << "#\n";
-		for (const auto &line : image)
-			output << '#' << line << "#\n";
-		output << '#' << string(size_.width, '#') << "#\n";
-	}
 private:
 	using Shapes = map<ShapeId, unique_ptr<IShape>>;
-
-	Shapes::iterator GetShapeNodeById(ShapeId id) {
-		auto it = shapes_.find(id);
-		if (it == shapes_.end()) {
-			throw std::out_of_range("No shape with given ID");
-		}
-		return it;
-	}
-
-	ShapeId InsertShape(unique_ptr<IShape> shape) {
-		shapes_[current_id_] = std::move(shape);
-		return current_id_++;
-	}
+	Shapes::iterator GetShapeNodeById(ShapeId id);
+	ShapeId InsertShape(unique_ptr<IShape> shape);
 
 	Size size_ = {};
 	ShapeId current_id_ = 0;
