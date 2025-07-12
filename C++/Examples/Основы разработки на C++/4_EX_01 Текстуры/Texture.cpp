35c2928b119303f218c59882fdfbb6d18c8402be
apkretov <apkretov@hotmail.com>
2025-07-12 13:43:05 +0300

    Texture.cpp

---
+++ C++/Examples/Основы разработки на C++/4_EX_01 Текстуры/Texture.cpp
@@ -0,0 +1,17 @@
+#include "Texture.h"
+#include <cassert>
+
+Texture::Texture(Image image) : image_(std::move(image)) {
+	for (const auto &line : image_)
+		assert(line.size() == image_[0].size());
+}
+
+Size Texture::GetSize() const {
+	auto width = static_cast<int>(image_.empty() ? 0 : image_[0].size());
+	auto height = static_cast<int>(image_.size());
+	return {width, height};
+}
+
+const Image &Texture::GetImage() const { 
+	return image_; 
+}
--- C++/Examples/Основы разработки на C++/4_EX_01 Текстуры/Texture.h
+++ C++/Examples/Основы разработки на C++/4_EX_01 Текстуры/Texture.h
@@ -1,22 +1,12 @@
 #pragma once
 
-#include <cassert>
 #include "common.h"
 #include "ITexture.h"
 
 class Texture : public ITexture {
 	Image image_;
 public:
-	Texture(Image image) : image_(std::move(image)) {
-		for (const auto &line : image_)
-			assert(line.size() == image_[0].size());
-	}
-
-	Size GetSize() const override {
-		auto width = static_cast<int>(image_.empty() ? 0 : image_[0].size());
-		auto height = static_cast<int>(image_.size());
-		return {width, height};
-	}
-
-	const Image &GetImage() const override { return image_; }
+	Texture(Image image);
+	Size GetSize() const override;
+	const Image &GetImage() const override;
 };
