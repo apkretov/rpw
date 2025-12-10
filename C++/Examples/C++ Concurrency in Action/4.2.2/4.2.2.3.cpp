876e159063b1cfb9d3ab52181d3388c8ac0b7392
apkretov <apkretov@hotmail.com>
2025-12-10 17:11:17 +0300

    C++ Concurrency in Action 4.2.2: refactoring

--- C++/Examples/C++ Concurrency in Action/4.2.2/4.2.2.3.cpp
+++ C++/Examples/C++ Concurrency in Action/4.2.2/4.2.2.3.cpp
@@ -4,20 +4,21 @@
 #include <mutex>
 #include <print>
 #include <thread>
-#include "../../stdafx.h"
+#include <source_location>
 #include "listing_4.9.h"
 using namespace std;
 
 const jthread gui_bg_thread(gui_thread);
 
 int main() {
-	print_file_line();
+	constexpr source_location loc = source_location::current();
+	print("File: {}\tLine: {}\n\n", loc.file_name(), loc.line()); // Explicitly replaces print_file_line() from stdafx.h because the GUI thread interrupts this printing.
 
-	auto fut1 = post_task_for_gui_thread([] { print("Thread {}: GUI Task 1: Updating button text.\n", this_thread::get_id()); }); // Post a few tasks
+	auto fut1 = post_task_for_gui_thread([] { print("Thread {}: GUI Task 1 - Updating button text.\n", this_thread::get_id()); }); // Post a few tasks
 	print("Thread {}: Posted Task 1 to GUI thread.\n", this_thread::get_id());
-	auto fut2 = post_task_for_gui_thread([] { print("Thread {}: GUI Task 2: Repainting window.\n", this_thread::get_id()); });
+	auto fut2 = post_task_for_gui_thread([] { print("Thread {}: GUI Task 2 - Repainting window.\n", this_thread::get_id()); });
 	print("Thread {}: Posted Task 2 to GUI thread.\n", this_thread::get_id());
-	auto fut3 = post_task_for_gui_thread([] { print("Thread {}: GUI Task 3: Updating status bar.\n", this_thread::get_id()); });
+	auto fut3 = post_task_for_gui_thread([] { print("Thread {}: GUI Task 3 - Updating status bar.\n", this_thread::get_id()); });
 	print("Thread {}: Posted Task 3 to GUI thread.\n\n", this_thread::get_id());
 
 	fut1.wait(); // Wait for tasks to complete
@@ -25,7 +26,7 @@ int main() {
 	fut3.wait();
 	print("Thread {}: All GUI tasks completed.\n\n", this_thread::get_id());
 
-	this_thread::sleep_for(chrono::milliseconds(100)); // Let GUI thread run a bit more
+	this_thread::sleep_for(chrono::milliseconds(50)); // Let GUI thread run a bit more
 	{
 		lock_guard lk(m);
 		shutdown_gui = true; // Shut down GUI
--- C++/Examples/C++ Concurrency in Action/4.2.2/listing_4.9.h
+++ C++/Examples/C++ Concurrency in Action/4.2.2/listing_4.9.h
@@ -16,25 +16,21 @@ bool gui_shutdown_message_received() {
 #pragma region MINE
 	std::lock_guard lk(m);
 	if (shutdown_gui)
-		print("Thread {}: GUI thread received a Shutdown message.\n", std::this_thread::get_id());
+		std::print("Thread {}: GUI thread received a Shutdown message.\n", std::this_thread::get_id());
 	return shutdown_gui;
 #pragma endregion
 }
 
 void get_and_process_gui_message() {
 #pragma region MINE
-	print("Thread {}: GUI thread processing its own messages...\n", std::this_thread::get_id());
+	std::print("Thread {}: GUI thread processing its own messages...\n", std::this_thread::get_id());
 	std::this_thread::sleep_for(std::chrono::milliseconds(10)); // Simulate GUI message processing
 #pragma endregion
 }
 
-#pragma region MINE
-// What is the difference between steps 3 and 6? To me, executing a task seems like processing messages. It feels like I’m mixing up different concepts.
-// In step 3, the GUI thread is processing its own messages, things like user input, clicks or window movements. Step 6 is actually executing tasks that were posted by another thread, like updating a button's text or redrawing the window. So, three is processing internal messages, and six is performing external tasks requested by other threads.
-#pragma endregion //MINE
 void gui_thread() {  // ❶
 	while (!gui_shutdown_message_received()) {  // ❷
-		get_and_process_gui_message();  // ❸
+		get_and_process_gui_message();  // ❸ // See Comment 1 below.
 		std::packaged_task<void()> task;
 		{
 			std::lock_guard<std::mutex> lk(m);
@@ -61,3 +57,9 @@ std::future<void> post_task_for_gui_thread(Func f) {
 #endif // MINE
 	return res;
 }
+
+#pragma region MINE
+// Comment 1.
+// What is the difference between steps 3 and 6? To me, executing a task seems like processing messages. It feels like I’m mixing up different concepts.
+// In step 3, the GUI thread is processing its own messages, things like user input, clicks or window movements. Step 6 is actually executing tasks that were posted by another thread, like updating a button's text or redrawing the window. So, three is processing internal messages, and six is performing external tasks requested by other threads.
+#pragma endregion //MINE
