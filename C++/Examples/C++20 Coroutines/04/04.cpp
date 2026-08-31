8f6340da34fc3c4b2758635652efbbec95369646
apkretov <apkretov@hotmail.com>
2026-08-31 18:28:22 +0300

    C++20 Coroutines 04

---
+++ C++/Examples/C++20 Coroutines/04/04.cpp
@@ -0,0 +1,43 @@
+#include <coroutine>
+#include <iostream>
+#include "../../stdafx.h"
+
+struct TracePromise {
+	struct promise_type {
+		promise_type() { std::cout << "promise constructed" << std::endl; }
+		~promise_type() { std::cout << "promise destroyed" << std::endl; }
+
+		TracePromise get_return_object() {
+			std::cout << "get_return_object called" << std::endl;
+			return {};
+		}
+
+		std::suspend_never initial_suspend() {
+			std::cout << "initial_suspend called" << std::endl;
+			return {};
+		}
+
+		std::suspend_always final_suspend() noexcept {
+			std::cout << "final_suspend called" << std::endl;
+			return {};
+		}
+
+		void return_void() { std::cout << "return_void called" << std::endl; }
+		void unhandled_exception() { std::cout << "unhandled_exception called" << std::endl; }
+	};
+
+	//ORIG std::coroutine_handle<promise_type> handle;
+};
+
+TracePromise trace_coroutine() {
+	std::cout << "coroutine body begins" << std::endl;
+	co_return;
+}
+
+int main() {
+	print_file_line();
+
+	std::cout << "calling coroutine" << std::endl;
+	auto result = trace_coroutine();
+	std::cout << "coroutine returned" << std::endl;
+}
